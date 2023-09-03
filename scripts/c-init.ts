52980a03c61edc2065f118a7798566725d1351f6
Craig Miller <craigmiller160@gmail.com>
2023-09-03 12:46:37 -0400

    More progress

--- scripts/c-init.ts
+++ scripts/c-init.ts
@@ -1,8 +1,11 @@
 import {findCwd} from './utils/cwd';
 import { function as func, either } from 'fp-ts';
+import path from 'path';
+import fs from 'fs';
 
 const performInitialization = (process: NodeJS.Process) => (cwd: string) => {
-
+    const theFile = path.join(cwd, 'foo.txt');
+    fs.writeFileSync(theFile, 'Hello World 123');
 };
 
 export const execute = (process: NodeJS.Process) => {
