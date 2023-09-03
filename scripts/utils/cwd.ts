0c6e99ed7f85c8788401fc06f108729b499bb593
Craig Miller <craigmiller160@gmail.com>
2023-09-03 12:36:08 -0400

    More progress with scripts

--- scripts/utils/cwd.ts
+++ scripts/utils/cwd.ts
@@ -1,21 +1,49 @@
 import path from 'path';
 import fs from 'fs';
 import {parsePackageJson} from './PackageJson';
-import {either, function as func} from 'fp-ts';
+import {either, function as func, option} from 'fp-ts';
+import { match } from 'ts-pattern';
+
+type CheckPathResult = 'js-config' | 'node_modules' | 'target-project' | 'invalid';
 
 export const JS_CONFIG_NAME = '@craigmiller160/js-config';
 
-export const findCwd = (process: NodeJS.Process): string => {
-    const localPackageJsonPath = path.join(process.cwd(), 'package.json');
-
-    if (fs.existsSync(localPackageJsonPath)) {
+const performPathCheck = (theDirectoryPath: string): CheckPathResult => {
+    const packageJsonPath = path.join(theDirectoryPath, 'package.json');
+    if (fs.existsSync(packageJsonPath)) {
         const name = func.pipe(
-            parsePackageJson(localPackageJsonPath),
+            parsePackageJson(packageJsonPath),
             either.fold(
                 () => '',
                 (packageJson) => packageJson.name
             )
         );
 
+        if (name === JS_CONFIG_NAME) {
+            return 'js-config';
+        }
+
+        if (name !== '' && name !== JS_CONFIG_NAME) {
+            return 'target-project';
+        }
     }
+
+    if (path.basename(theDirectoryPath) === 'node_modules') {
+        return 'node_modules';
+    }
+
+    return 'invalid';
 };
+
+export const checkPath = (theDirectoryPath: string): either.Either<Error, string> => {
+    const result = performPathCheck(theDirectoryPath);
+    return match(result)
+        .with('target-project', () => either.right(theDirectoryPath))
+        .with('js-config', () => checkPath(path.join(theDirectoryPath, '..', '..')))
+        .with('node_modules', () => checkPath(path.join(theDirectoryPath, '..')))
+        .with('invalid', () => either.left(new Error(`Path is invalid: ${theDirectoryPath}`)))
+        .exhaustive();
+};
+
+export const findCwd = (process: NodeJS.Process): either.Either<Error, string> =>
+    checkPath(process.cwd());
