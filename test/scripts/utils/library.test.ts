7d0572f71c80d15d4f7dcdcd8d520f0167da7b56
Craig Miller <craigmiller160@gmail.com>
2023-09-04 17:29:36 -0400

    Cleanup

--- test/scripts/utils/library.test.ts
+++ test/scripts/utils/library.test.ts
@@ -1,8 +1,20 @@
 import {describe, expect, it} from 'vitest';
 import path from 'path';
 import {isLibraryPresent} from '../../../scripts/utils/library';
+import {PackageJson} from '../../../scripts/utils/PackageJson';
 
 const WORKING_DIR = path.join(process.cwd(), 'test', '__working_directories__', 'isLibraryPresent');
+
+const packageJson: PackageJson = {
+    name: 'my-project',
+    version: '1.0.0',
+    dependencies: {
+        myLib: '1.0.0'
+    },
+    devDependencies: {
+        myOtherLib: '2.0.0'
+    }
+}
 
 describe('library', () => {
     describe('isLibraryPresent', () => {
@@ -17,19 +29,23 @@ describe('library', () => {
         })
 
         it('is present, using full PackageJson and string', () => {
-            throw new Error();
+            const result = isLibraryPresent(packageJson, 'myLib');
+            expect(result).toEqual(true);
         })
 
         it('is not present, using full PackageJson and string', () => {
-            throw new Error();
+            const result = isLibraryPresent(packageJson, 'myLib2');
+            expect(result).toEqual(false);
         })
 
         it('is present, using full PackageJson and regex', () => {
-            throw new Error();
+            const result = isLibraryPresent(packageJson, /^myOther.*$/);
+            expect(result).toEqual(true);
         })
 
         it('is not present, using full PackageJson and regex', () => {
-            throw new Error();
+            const result = isLibraryPresent(packageJson, /^myOther2.*$/);
+            expect(result).toEqual(false);
         })
     });
 })
