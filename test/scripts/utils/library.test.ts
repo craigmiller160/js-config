cadff3dcd7a0476777e0201deb18e2d9afbfba17
Craig Miller <craigmiller160@gmail.com>
2023-09-04 17:05:09 -0400

    Stubbed out new tests

---
+++ test/scripts/utils/library.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from 'vitest';
+
+describe('library', () => {
+    describe('isLibraryPresent', () => {
+        it('is present, using the CWD and string', () => {
+            throw new Error();
+        })
+
+        it('is not present, using the CWD and string', () => {
+            throw new Error();
+        })
+
+        it('is present, using full PackageJson and string', () => {
+            throw new Error();
+        })
+
+        it('is not present, using full PackageJson and string', () => {
+            throw new Error();
+        })
+
+        it('is present, using full PackageJson and regex', () => {
+            throw new Error();
+        })
+
+        it('is not present, using full PackageJson and regex', () => {
+            throw new Error();
+        })
+    });
+})
