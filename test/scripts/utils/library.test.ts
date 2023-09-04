948862ed6dff7eaf9c38b80b9be6d9eccc7c5dc3
Craig Miller <craigmiller160@gmail.com>
2023-09-04 17:09:32 -0400

    Implementing test logic

--- test/scripts/utils/library.test.ts
+++ test/scripts/utils/library.test.ts
@@ -1,13 +1,19 @@
-import { describe, it } from 'vitest';
+import {describe, expect, it} from 'vitest';
+import path from 'path';
+import {isLibraryPresent} from '../../../scripts/utils/library';
+
+const WORKING_DIR = path.join(process.cwd(), 'test', '__working_directories__', 'isLibraryPresent');
 
 describe('library', () => {
     describe('isLibraryPresent', () => {
         it('is present, using the CWD and string', () => {
-            throw new Error();
+            const result = isLibraryPresent(path.join(WORKING_DIR, 'package.json'), 'my-lib');
+            expect(result).toEqual(true);
         })
 
         it('is not present, using the CWD and string', () => {
-            throw new Error();
+            const result = isLibraryPresent(path.join(WORKING_DIR, 'package.json'), 'my-lib2');
+            expect(result).toEqual(false);
         })
 
         it('is present, using full PackageJson and string', () => {
