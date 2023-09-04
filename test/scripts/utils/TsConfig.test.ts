6ecab37bfc4a0ca236123f7254edc8b77b0aae43
Craig Miller <craigmiller160@gmail.com>
2023-09-04 14:45:49 -0400

    Implemented test

--- test/scripts/utils/TsConfig.test.ts
+++ test/scripts/utils/TsConfig.test.ts
@@ -1,7 +1,24 @@
 import { describe, it, expect } from 'vitest';
+import path from 'path';
+import {parseTsConfig} from '../../../scripts/utils/TsConfig';
 
 describe('TsConfig', () => {
     it('parses tsconfig.json', () => {
-        throw new Error();
+        const tsConfigPath = path.join(process.cwd(), 'test', '__working_directories__', 'TsConfig', 'tsconfig.json');
+        const result = parseTsConfig(tsConfigPath);
+        expect(result).toEqualRight({
+            extends: './configs/typescript/tsconfig.json',
+            compilerOptions: {
+                module: 'es2020'
+            },
+            include: [
+                'scripts/**/*',
+                'vitest.config.mts'
+            ],
+            exclude: [
+                'node_modules',
+                'build'
+            ]
+        })
     });
 });
