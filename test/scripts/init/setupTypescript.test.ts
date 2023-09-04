6412a5a5c300ab8f107b7e4eb066da05622f78dc
Craig Miller <craigmiller160@gmail.com>
2023-09-03 23:05:07 -0400

    Stubbing out more tests

---
+++ test/scripts/init/setupTypescript.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+
+describe('setupTypescript', () => {
+    it('writes tsconfig.json project without one', () => {
+        throw new Error();
+    });
+
+    it('writes tsconfig.json, preserving compilerOptions from existing one', () => {
+        throw new Error();
+    });
+});
