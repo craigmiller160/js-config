6209c35fd7d3f96672c2422d416d7ce814712baf
Craig Miller <craigmiller160@gmail.com>
2023-09-04 14:55:24 -0400

    Test setup

--- test/scripts/init/setupTypescript.test.ts
+++ test/scripts/init/setupTypescript.test.ts
@@ -1,4 +1,7 @@
 import { describe, it } from 'vitest';
+import path from 'path';
+
+const WORKING_DIR_PATH = path.join(process.cwd(), 'test', '__working_directories__', 'typescript');
 
 describe('setupTypescript', () => {
     it('writes tsconfig.json to a project without one', () => {
