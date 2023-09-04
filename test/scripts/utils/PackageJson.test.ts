cfc83102bee351ca9176b77e53b448773351d396
Craig Miller <craigmiller160@gmail.com>
2023-09-03 23:09:29 -0400

    First test implemented

--- test/scripts/utils/PackageJson.test.ts
+++ test/scripts/utils/PackageJson.test.ts
@@ -1,8 +1,14 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import {packageJsonCodec} from '../../../scripts/utils/PackageJson';
 
 describe('PackageJson', () => {
     it('parses a valid package.json', () => {
-        throw new Error();
+        const json = {
+            name: 'TheName',
+            version: '1.0.0'
+        }
+        const result = packageJsonCodec.decode(json);
+        expect(result).toEqualRight(json);
     });
 
     it('returns errors for invalid package.json', () => {
