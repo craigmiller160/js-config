12d1c0d1c3095fc3ff0c47c57f6e653b2d888650
Craig Miller <craigmiller160@gmail.com>
2023-09-04 14:52:19 -0400

    Cleanup

--- scripts/init/setupTypescript.ts
+++ scripts/init/setupTypescript.ts
@@ -2,13 +2,8 @@ import fs from 'fs';
 import { readonlyArray, function as func, either } from 'fp-ts';
 import path from 'path';
 import {unknownToError} from '../utils/unknownToError';
-
-type TsConfig = Readonly<{
-    extends: string;
-    compilerOptions?: Readonly<{}>;
-    include: ReadonlyArray<string>;
-    exclude: ReadonlyArray<string>;
-}>;
+import {parseTsConfig, TsConfig} from '../utils/TsConfig';
+import {logger} from '../logger';
 
 const ADDITIONAL_FILES: ReadonlyArray<RegExp> = [
     /^vite\.config\.[cm]ts$/,
@@ -28,28 +23,47 @@ const findAdditionalFiles = (cwd: string): ReadonlyArray<string> =>
         readonlyArray.filter(isAdditionalFile)
     );
 
-export const setupTypescript = (cwd: string): either.Either<Error, void> =>
-    either.tryCatch(() => {
-        const additionalFiles = findAdditionalFiles(cwd);
-        const rootTsConfigPath = path.join(cwd, 'tsconfig.json');
-        let existingRootTsConfig: TsConfig | undefined = undefined;
-        if (fs.existsSync(rootTsConfigPath)) {
-            existingRootTsConfig = JSON.parse(fs.readFileSync(rootTsConfigPath, 'utf8')) as TsConfig;
-        }
+export const setupTypescript = (cwd: string): either.Either<Error, void> => {
+    logger.info('Setting up TypeScript');
+    const additionalFiles: ReadonlyArray<string> = func.pipe(
+        either.tryCatch(() => findAdditionalFiles(cwd), unknownToError),
+        either.fold(
+            (error) => {
+                logger.error(`Error finding additional files for tsconfig: ${error}`)
+                return []
+            },
+            func.identity
+        )
+    );
 
-        const rootTsConfig: TsConfig = {
-            extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
-            compilerOptions: existingRootTsConfig?.compilerOptions,
-            include: [
-                'src/**/*',
-                ...additionalFiles
-            ],
-            exclude: [
-                'node_modules',
-                'build',
-                'lib'
-            ]
-        };
+    const rootTsConfigPath = path.join(cwd, 'tsconfig.json');
+    let existingRootTsConfig: TsConfig | undefined = undefined;
+    if (fs.existsSync(rootTsConfigPath)) {
+        existingRootTsConfig = func.pipe(
+            parseTsConfig(rootTsConfigPath),
+            either.fold(
+                (error): TsConfig | undefined => {
+                    logger.error(`Error parsing root tsconfig.json: ${error}`);
+                    return undefined;
+                },
+                func.identity
+            )
+        );
+    }
 
-        fs.writeFileSync(rootTsConfigPath, JSON.stringify(rootTsConfig, null, 2));
-    }, unknownToError);
+    const rootTsConfig: TsConfig = {
+        extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
+        compilerOptions: existingRootTsConfig?.compilerOptions,
+        include: [
+            'src/**/*',
+            ...additionalFiles
+        ],
+        exclude: [
+            'node_modules',
+            'build',
+            'lib'
+        ]
+    };
+
+    return either.tryCatch(() => fs.writeFileSync(rootTsConfigPath, JSON.stringify(rootTsConfig, null, 2)), unknownToError);
+};
