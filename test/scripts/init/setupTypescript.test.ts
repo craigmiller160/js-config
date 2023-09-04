a715c8e7dc3c1d6c4d1b9bd0124c20f811d3ee9e
Craig Miller <craigmiller160@gmail.com>
2023-09-04 14:57:47 -0400

    Setup test clean logic

--- test/scripts/init/setupTypescript.test.ts
+++ test/scripts/init/setupTypescript.test.ts
@@ -1,9 +1,31 @@
-import { describe, it } from 'vitest';
+import {afterEach, beforeEach, describe, it} from 'vitest';
 import path from 'path';
+import fs from 'fs';
+import {isUtf8} from 'buffer';
 
 const WORKING_DIR_PATH = path.join(process.cwd(), 'test', '__working_directories__', 'typescript');
 
+const resetWorkingDirectory = () => {
+    fs.readdirSync(WORKING_DIR_PATH)
+        .filter((fileName) => !['.gitignore', '.gitkeep'].includes(fileName))
+        .forEach((fileName) => {
+            const fullPath = path.join(WORKING_DIR_PATH, fileName);
+            fs.rmSync(fullPath, {
+                recursive: true,
+                force: true
+            });
+        });
+};
+
 describe('setupTypescript', () => {
+    beforeEach(() => {
+        resetWorkingDirectory();
+    });
+
+    afterEach(() => {
+        resetWorkingDirectory();
+    });
+
     it('writes tsconfig.json to a project without one', () => {
         throw new Error();
     });
