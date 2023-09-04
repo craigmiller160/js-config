8cd38de4bce7327d82c9083cc756e82d7d540783
Craig Miller <craigmiller160@gmail.com>
2023-09-03 23:04:04 -0400

    Stubbed out tests

---
+++ test/scripts/utils/PackageJson.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+
+describe('PackageJson', () => {
+    it('parses a valid package.json', () => {
+        throw new Error();
+    });
+
+    it('returns errors for invalid package.json', () => {
+        throw new Error();
+    });
+});
---
+++ test/scripts/utils/cwd.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from 'vitest';
+
+describe('cwd', () => {
+    describe('findCwd', () => {
+       it('is js-config library itself', () => {
+           throw new Error();
+       });
+
+       it('is added directly to project', () => {
+           throw new Error();
+       });
+
+       it('is added directly to project via pnpm', () => {
+           throw new Error();
+       });
+
+       it('is invalid', () => {
+           throw new Error();
+       });
+    });
+});
