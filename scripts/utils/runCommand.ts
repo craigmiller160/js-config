352ebe618f797dc93afb5a16235e56baa864e3c2
Craig Miller <craigmiller160@gmail.com>
2023-09-05 13:25:20 -0400

    Implemented command

--- scripts/utils/runCommand.ts
+++ scripts/utils/runCommand.ts
@@ -4,5 +4,12 @@ import { either } from 'fp-ts';
 
 export const runCommandSync = (command: string, options?: SpawnOptions): either.Either<Error, string> => {
     const commandParts = command.split(' ');
-    spawnSync(commandParts[0], commandParts.slice(1), options);
+    const result = spawnSync(commandParts[0], commandParts.slice(1), {
+        ...(options ?? {}),
+        stdio: options?.stdio ?? 'inherit'
+    });
+    if (result.status === 0) {
+        return either.right(result.stdout.toString('utf8'));
+    }
+    return either.left(new Error(`Command failed. Status: ${result.status} Message: ${result.stderr.toString('utf8')}`));
 };
