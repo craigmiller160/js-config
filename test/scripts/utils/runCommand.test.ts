8b9b70b7b420f96ce7888bc00c17ca67a4d4a167
Craig Miller <craigmiller160@gmail.com>
2023-09-05 15:00:38 -0400

    Stubbed out tests

---
+++ test/scripts/utils/runCommand.test.ts
@@ -0,0 +1,21 @@
+import {describe, it} from 'vitest';
+
+describe('runCommand', () => {
+    describe('runCommandSync', () => {
+        it('runs command successfully with default options', () => {
+            throw new Error();
+        });
+
+        it('runs command successfully and returns output', () => {
+            throw new Error();
+        });
+
+        it('runs command with error with default options', () => {
+            throw new Error();
+        })
+
+        it('runs command with error and returns output', () => {
+            throw new Error();
+        })
+    })
+})
