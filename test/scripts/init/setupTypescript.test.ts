f4c57f27715b72f11bff14428f2f2abb92ce7a42
Craig Miller <craigmiller160@gmail.com>
2023-09-07 11:48:10 -0400

    Stubbed out new tests

--- test/scripts/init/setupTypescript.test.ts
+++ test/scripts/init/setupTypescript.test.ts
@@ -34,72 +34,84 @@ describe('setupTypescript', () => {
         resetWorkingDirectory();
     });
 
-    it('writes tsconfig.json to a project without one', () => {
-        const result = setupTypescript(WORKING_DIR_PATH);
-        expect(result).toBeRight();
-        const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
-        expect(fs.existsSync(tsConfigPath)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
-            extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
-            include: [
-                'src/**/*'
-            ],
-            exclude: [
-                'node_modules',
-                'build',
-                'lib'
-            ]
+    describe('base tsconfig.json', () => {
+        it('writes tsconfig.json to a project without one', () => {
+            const result = setupTypescript(WORKING_DIR_PATH);
+            expect(result).toBeRight();
+            const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
+            expect(fs.existsSync(tsConfigPath)).toEqual(true);
+            expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
+                extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
+                include: [
+                    'src/**/*'
+                ],
+                exclude: [
+                    'node_modules',
+                    'build',
+                    'lib'
+                ]
+            });
+        });
+
+        it('writes tsconfig.json to a project without one, adding additional files', () => {
+            ADDITIONAL_FILES.forEach((fileName) => {
+                const fullPath = path.join(WORKING_DIR_PATH, fileName);
+                fs.writeFileSync(fullPath, 'a');
+            });
+            const result = setupTypescript(WORKING_DIR_PATH);
+            expect(result).toBeRight();
+
+            const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
+            expect(fs.existsSync(tsConfigPath)).toEqual(true);
+            expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
+                extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
+                include: [
+                    'src/**/*',
+                    ...ADDITIONAL_FILES.sort()
+                ],
+                exclude: [
+                    'node_modules',
+                    'build',
+                    'lib'
+                ]
+            });
+        });
+
+        it('writes tsconfig.json, preserving compilerOptions from existing one', () => {
+            const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
+            fs.writeFileSync(tsConfigPath, JSON.stringify({
+                compilerOptions: {
+                    module: 'es2020'
+                }
+            }));
+
+            const result = setupTypescript(WORKING_DIR_PATH);
+            expect(result).toBeRight();
+            expect(fs.existsSync(tsConfigPath)).toEqual(true);
+            expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
+                extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
+                compilerOptions: {
+                    module: 'es2020'
+                },
+                include: [
+                    'src/**/*'
+                ],
+                exclude: [
+                    'node_modules',
+                    'build',
+                    'lib'
+                ]
+            });
         });
     });
 
-    it('writes tsconfig.json to a project without one, adding additional files', () => {
-        ADDITIONAL_FILES.forEach((fileName) => {
-            const fullPath = path.join(WORKING_DIR_PATH, fileName);
-            fs.writeFileSync(fullPath, 'a');
+    describe('test tsconfig.json', () => {
+        it('writes test/tsconfig.json to project without one', () => {
+            throw new Error();
         });
-        const result = setupTypescript(WORKING_DIR_PATH);
-        expect(result).toBeRight();
 
-        const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
-        expect(fs.existsSync(tsConfigPath)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
-            extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
-            include: [
-                'src/**/*',
-                ...ADDITIONAL_FILES.sort()
-            ],
-            exclude: [
-                'node_modules',
-                'build',
-                'lib'
-            ]
+        it('writes test/tsconfig.json to project with one, preserving compilerOptions', () => {
+            throw new Error();
         });
-    });
-
-    it('writes tsconfig.json, preserving compilerOptions from existing one', () => {
-        const tsConfigPath = path.join(WORKING_DIR_PATH, 'tsconfig.json');
-        fs.writeFileSync(tsConfigPath, JSON.stringify({
-            compilerOptions: {
-                module: 'es2020'
-            }
-        }));
-
-        const result = setupTypescript(WORKING_DIR_PATH);
-        expect(result).toBeRight();
-        expect(fs.existsSync(tsConfigPath)).toEqual(true);
-        expect(JSON.parse(fs.readFileSync(tsConfigPath, 'utf8'))).toEqual({
-            extends: '@craigmiller160/js-config/configs/typescript/tsconfig.json',
-            compilerOptions: {
-                module: 'es2020'
-            },
-            include: [
-                'src/**/*'
-            ],
-            exclude: [
-                'node_modules',
-                'build',
-                'lib'
-            ]
-        });
-    });
+    })
 });
