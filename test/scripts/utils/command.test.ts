80016f1f6c9c84b9eedbd93adf64116ea9d8aca9
Craig Miller <craigmiller160@gmail.com>
2023-09-07 17:00:35 -0400

    More work setting up tests

--- test/scripts/utils/command.test.ts
+++ test/scripts/utils/command.test.ts
@@ -1,5 +1,14 @@
 import { describe, it } from 'vitest';
 
+const NODE_PATH = [
+    '.pnpm/js-config-root/node_modules/@craigmiller160/js-config/build/bin/node_modules',
+    '.pnpm/js-config-root/node_modules/@craigmiller160/js-config/build/node_modules',
+    '.pnpm/js-config-root/node_modules/@craigmiller160/js-config/node_modules',
+    '.pnpm/js-config-root/node_modules/@craigmiller160/node_modules',
+    '.pnpm/js-config-root/node_modules',
+    '.pnpm/node_modules'
+];
+
 describe('command', () => {
     describe('findCommand', () => {
         it('finds command from NODE_PATH', () => {
