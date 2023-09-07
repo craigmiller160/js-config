9c32534c21d0def64b308caeeb127470b2c42baa
Craig Miller <craigmiller160@gmail.com>
2023-09-07 16:01:01 -0400

    Fixing script

--- scripts/c-type-check.ts
+++ scripts/c-type-check.ts
@@ -3,27 +3,38 @@ import path from 'path';
 import fs from 'fs';
 import {logger} from './logger';
 import {terminate} from './utils/terminate';
+import {either, function as func} from 'fp-ts';
+
+const runRootTypeCheck = (): either.Either<Error, unknown> => {
+    const testTsconfigPath = path.join(process.cwd(), 'test', 'tsconfig.json');
+    if (fs.existsSync(testTsconfigPath)) {
+        logger.debug('Using test tsconfig.json for type check');
+        return runCommandSync('tsc --noEmit --project ./test/tsconfig.json');
+    }
+
+    logger.debug('Using base tsconfig.json for type check');
+    return runCommandSync('tsc --noEmit');
+};
+
+const runCypressTypeCheck = (): either.Either<Error, unknown> => {
+    const cypressTsconfigPath = path.join(process.cwd(), 'cypress', 'tsconfig.json');
+    if (fs.existsSync(cypressTsconfigPath)) {
+        logger.debug('Cypress detected, performing cypress type check');
+        return runCommandSync('tsc --noEmit --project ./cypress/tsconfig.json');
+    }
+
+    logger.debug('Cypress not present, skipping cypress type check');
+    return either.right(func.constVoid());
+};
 
 export const execute = (process: NodeJS.Process) => {
-    try {
-        logger.info('Performing typescript type check');
-        const testTsconfigPath = path.join(process.cwd(), 'test', 'tsconfig.json');
-        if (fs.existsSync(testTsconfigPath)) {
-            logger.debug('Using test tsconfig.json for type check');
-            runCommandSync('tsc --noEmit --project ./test/tsconfig.json');
-        } else {
-            logger.debug('Using base tsconfig.json for type check');
-            runCommandSync('tsc --noEmit');
-        }
-
-        const cypressTsconfigPath = path.join(process.cwd(), 'cypress', 'tsconfig.json');
-        if (fs.existsSync(cypressTsconfigPath)) {
-            logger.debug('Cypress detected, performing cypress type check');
-            runCommandSync('tsc --noEmit --project ./cypress/tsconfig.json');
-        } else {
-            logger.debug('Cypress not present, skipping cypress type check');
-        }
-    } catch (ex) {
-        terminate(ex);
-    }
+    logger.info('Performing typescript type check');
+    return func.pipe(
+        runRootTypeCheck(),
+        either.chain(() => runCypressTypeCheck()),
+        either.fold(
+            terminate,
+            terminate
+        )
+    );
 };
