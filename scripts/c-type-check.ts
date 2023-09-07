aec35f044e67dc5bcfde92aa8719da360dedcb71
Craig Miller <craigmiller160@gmail.com>
2023-09-07 17:10:24 -0400

    Updated type check script

--- scripts/c-type-check.ts
+++ scripts/c-type-check.ts
@@ -4,23 +4,24 @@ import fs from 'fs';
 import {logger} from './logger';
 import {terminate} from './utils/terminate';
 import {either, function as func} from 'fp-ts';
+import {findCommand} from './utils/command';
 
-const runRootTypeCheck = (): either.Either<Error, unknown> => {
+const runRootTypeCheck = (command: string): either.Either<Error, unknown> => {
     const testTsconfigPath = path.join(process.cwd(), 'test', 'tsconfig.json');
     if (fs.existsSync(testTsconfigPath)) {
         logger.debug('Using test tsconfig.json for type check');
-        return runCommandSync('tsc --noEmit --project ./test/tsconfig.json');
+        return runCommandSync(`${command} --noEmit --project ./test/tsconfig.json`);
     }
 
     logger.debug('Using base tsconfig.json for type check');
     return runCommandSync('tsc --noEmit');
 };
 
-const runCypressTypeCheck = (): either.Either<Error, unknown> => {
+const runCypressTypeCheck = (command: string): either.Either<Error, unknown> => {
     const cypressTsconfigPath = path.join(process.cwd(), 'cypress', 'tsconfig.json');
     if (fs.existsSync(cypressTsconfigPath)) {
         logger.debug('Cypress detected, performing cypress type check');
-        return runCommandSync('tsc --noEmit --project ./cypress/tsconfig.json');
+        return runCommandSync(`${command} --noEmit --project ./cypress/tsconfig.json`);
     }
 
     logger.debug('Cypress not present, skipping cypress type check');
@@ -29,9 +30,12 @@ const runCypressTypeCheck = (): either.Either<Error, unknown> => {
 
 export const execute = (process: NodeJS.Process) => {
     logger.info('Performing typescript type check');
+
     func.pipe(
-        runRootTypeCheck(),
-        either.chain(() => runCypressTypeCheck()),
+        findCommand(process, 'typescript/bin/tsc'),
+        either.bindTo('command'),
+        either.chainFirst(({ command }) => runRootTypeCheck(command)),
+        either.chainFirst(({ command }) => runCypressTypeCheck(command)),
         either.fold(
             terminate,
             terminate
