e422d6faf50615e1b519afbb43089c729d2b1df8
Craig Miller <craigmiller160@gmail.com>
2023-09-07 17:07:44 -0400

    Implemented function

--- scripts/utils/command.ts
+++ scripts/utils/command.ts
@@ -1,5 +1,14 @@
 import {either} from 'fp-ts';
+import path from 'path';
+import fs from 'fs';
 
 export const findCommand = (process: NodeJS.Process, pathFromNodeModules: string): either.Either<Error, string> => {
+    const matchingPaths = process.env.NODE_PATH.split(':')
+        .map((thePath) => path.join(thePath, pathFromNodeModules))
+        .filter((theFullPath) => fs.existsSync(theFullPath));
 
+    if (matchingPaths.length === 0) {
+        return either.left(new Error(`Unable to find command on NODE_PATH: ${pathFromNodeModules}`));
+    }
+    return either.right(matchingPaths[0]);
 };
