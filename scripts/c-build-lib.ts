c72db736cb16726fa2797957ae7752997577ee43
Craig Miller <craigmiller160@gmail.com>
2023-09-07 16:05:54 -0400

    Working on some logic and stubbing out test

--- scripts/c-build-lib.ts
+++ scripts/c-build-lib.ts
@@ -1,3 +1,18 @@
-export const execute = (process: NodeJS.Process) => {
+import path from 'path';
+import fs from 'fs';
+import {logger} from './logger';
+import {runCommandSync} from './utils/runCommand';
 
+
+export const execute = (process: NodeJS.Process) => {
+    logger.info('Performing library build');
+    const libDir = path.join(process.cwd(), 'lib');
+    if (fs.existsSync(libDir)) {
+        fs.rmSync(libDir, {
+            recursive: true,
+            force: true
+        });
+    }
+
+    runCommandSync('')
 };
---
+++ test/scripts/c-build-lib.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'vitest';
+
+describe('c-build-lib', () => {
+    it('runs build', () => {
+        throw new Error();
+    });
+});
