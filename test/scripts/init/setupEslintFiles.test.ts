ea4be03cf2ed4ae5fbf8b72bc1364bd601012544
Craig Miller <craigmiller160@gmail.com>
2023-09-08 12:03:37 -0400

    Test setup work

--- test/scripts/init/setupEslintFiles.test.ts
+++ test/scripts/init/setupEslintFiles.test.ts
@@ -1,6 +1,35 @@
-import { describe, it } from 'vitest';
+import { afterEach, beforeEach, describe, it } from 'vitest';
+import path from 'path';
+import fs from 'fs';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'eslint'
+);
+
+const wipeWorkingDir = () =>
+	fs
+		.readdirSync(WORKING_DIR)
+		.filter((fileName) => '.gitkeep' !== fileName)
+		.map((fileName) => path.join(WORKING_DIR, fileName))
+		.forEach((filePath) =>
+			fs.rmSync(filePath, {
+				recursive: true,
+				force: true
+			})
+		);
 
 describe('setupEslint', () => {
+	beforeEach(() => {
+		wipeWorkingDir();
+	});
+
+	afterEach(() => {
+		wipeWorkingDir();
+	});
+
 	it('writes default eslint & prettier config files when none exist', () => {
 		throw new Error();
 	});
