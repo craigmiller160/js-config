1e1c5f21cbd694be642d0ebfeaa8067557db64de
Craig Miller <craigmiller160@gmail.com>
2023-09-08 12:20:52 -0400

    Working on implementation

--- scripts/init/setupEslintFiles.ts
+++ scripts/init/setupEslintFiles.ts
@@ -1,5 +1,33 @@
-import {either} from 'fp-ts';
+import { either } from 'fp-ts';
+import path from 'path';
+import fs from 'fs';
+
+const getEslintrcPath = (cwd: string): string => path.join(cwd, '.eslintrc.js');
+const getPrettierrcPath = (cwd: string): string =>
+	path.join(cwd, '.prettierrc.js');
+
+const shouldWriteConfig = (configPath: string): boolean => {
+	if (fs.existsSync(configPath)) {
+		const config = fs.readFileSync(configPath, 'utf8');
+		return !config.includes('@craigmiller160/js-config');
+	}
+	return true;
+};
 
 export const setupEslintFiles = (cwd: string): either.Either<Error, void> => {
-	throw new Error();
+	const eslintrcPath = getEslintrcPath(cwd);
+	if (shouldWriteConfig(eslintrcPath)) {
+		fs.writeFileSync(
+			eslintrcPath,
+			`module.exports = require('@craigmiller160/js-config/configs/eslint/.eslintrc.js');`
+		);
+	}
+
+	const prettierrcPath = getPrettierrcPath(cwd);
+	if (shouldWriteConfig(prettierrcPath)) {
+		fs.writeFileSync(
+			prettierrcPath,
+			`module.exports = require('@craigmiller160/js-config/configs/eslint/.prettierrc.js');`
+		);
+	}
 };
