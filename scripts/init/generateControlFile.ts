963482becc9841cbb3ea3ecdc275e99b6cd78988
Craig Miller <craigmiller160@gmail.com>
2023-09-08 11:17:40 -0400

    Adjusted control file generation

--- scripts/init/generateControlFile.ts
+++ scripts/init/generateControlFile.ts
@@ -6,7 +6,8 @@ import {PackageJson} from '../files/PackageJson';
 
 export const generateControlFile = (cwd: string, packageJson: PackageJson, process: NodeJS.Process): either.Either<Error, void> => {
     const controlFile: ControlFile = {
-        workingDirectoryPath: cwd
+        workingDirectoryPath: cwd,
+        projectType: packageJson.type ?? 'commonjs'
     };
     return either.tryCatch(() => fs.writeFileSync(getControlFilePath(process.cwd()), JSON.stringify(controlFile, null, 2)), unknownToError);
 };
