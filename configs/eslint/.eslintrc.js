15dc42be6f08228e82f54608b18379765d06f4b2
Craig Miller <craigmiller160@gmail.com>
2023-09-08 11:48:03 -0400

    Got eslint working-ish

--- configs/eslint/.eslintrc.js
+++ configs/eslint/.eslintrc.js
@@ -1,26 +1,19 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:prettier/recommended'
-    ],
-    rules: {
-        'no-console': [
-            'error',
-            {
-                allow: ['error']
-            }
-        ],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }]
-    },
-    overrides: [
-        {
-            files: [
-                '**/*.[cm]?ts?(x)'
-            ],
-            parser: '@typescript-eslint/parser',
-            extends: [
-                'plugin:@typescript-eslint/recommended'
-            ]
-        }
-    ]
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+	rules: {
+		'no-console': [
+			'error',
+			{
+				allow: ['error']
+			}
+		],
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+	},
+	overrides: [
+		{
+			files: ['**/*.[cm]?ts?(x)'],
+			parser: '@typescript-eslint/parser',
+			extends: ['plugin:@typescript-eslint/recommended']
+		}
+	]
 };
