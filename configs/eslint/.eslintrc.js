3c1c0a8d51e6cb403b5e3dda75b866037d118da4
Craig Miller <craigmiller160@gmail.com>
2023-09-07 22:07:45 -0400

    Working on eslint config

---
+++ configs/eslint/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': [
+            'error',
+            {
+                allow: ['error']
+            }
+        ]
+    }
+};
