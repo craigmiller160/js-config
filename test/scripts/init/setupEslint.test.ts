a3292277a1fda26264cfc01f28e19fec40461dac
Craig Miller <craigmiller160@gmail.com>
2023-09-08 11:54:34 -0400

    Stubbed out propper tests

--- test/scripts/init/setupEslint.test.ts
+++ test/scripts/init/setupEslint.test.ts
@@ -1,7 +1,15 @@
-import {describe, it} from 'vitest';
+import { describe, it } from 'vitest';
 
 describe('setupEslint', () => {
-    it('test', () => {
-        throw new Error();
-    })
-})
+	it('writes default eslint & prettier config files when none exist', () => {
+		throw new Error();
+	});
+
+	it("writes default eslint & prettier config files, replacing existing ones that don't reference this lib", () => {
+		throw new Error();
+	});
+
+	it('do nothing when eslint & prettier config files that reference this lib exist', () => {
+		throw new Error();
+	});
+});
