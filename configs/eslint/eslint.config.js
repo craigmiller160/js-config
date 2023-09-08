20819cab7dd8a008cf67a760c716ead1eb02a05c
Craig Miller <craigmiller160@gmail.com>
2023-09-08 11:38:52 -0400

    Progress

---
+++ configs/eslint/eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        extends: [
+            'eslint:recommended',
+            'plugin:prettier/recommended'
+        ],
+        rules: {
+            'no-console': [
+                'error',
+                {
+                    allow: ['error']
+                }
+            ],
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+        }
+    }
+]
