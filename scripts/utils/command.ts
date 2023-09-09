d203999d62337f0daf5ad96fb7290eff97af0740
Craig Miller <craigmiller160@gmail.com>
2023-09-09 11:30:39 -0400

    Refactoring findCommand

--- scripts/utils/command.ts
+++ scripts/utils/command.ts
@@ -1,21 +1,40 @@
-import { either } from 'fp-ts';
+import { either, option, function as func } from 'fp-ts';
 import path from 'path';
 import fs from 'fs';
 
-export const findCommand = (
+const PNPM_PATH_REGEX = /(?<pnpmPath>^.*\/\.pnpm\/).*$/;
+
+const findViaNodePath = (
 	process: NodeJS.Process,
 	pathFromNodeModules: string
-): either.Either<Error, string> => {
+): option.Option<string> => {
 	const matchingPaths = (process.env.NODE_PATH ?? '')
 		.split(':')
 		.map((thePath) => path.join(thePath, pathFromNodeModules))
 		.filter((theFullPath) => fs.existsSync(theFullPath));
 
 	if (matchingPaths.length > 0) {
-		return either.right(matchingPaths[0]);
+		return option.some(matchingPaths[0]);
 	}
-
-	return either.left(
-		new Error(`Unable to find command on NODE_PATH: ${pathFromNodeModules}`)
-	);
+	return option.none;
 };
+
+const findViaPnpmPath = (
+	process: NodeJS.Process,
+	pathFromNodeModules: string
+): option.Option<string> => {};
+
+export const findCommand = (
+	process: NodeJS.Process,
+	pathFromNodeModules: string
+): either.Either<Error, string> =>
+	func.pipe(
+		findViaNodePath(process, pathFromNodeModules),
+		option.fold(
+			() => findViaPnpmPath(process, pathFromNodeModules),
+			(thePath) => option.some(thePath)
+		),
+		either.fromOption(
+			() => new Error(`Unable to find command: ${pathFromNodeModules}`)
+		)
+	);
