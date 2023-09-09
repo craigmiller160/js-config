99011083532bc33a98a6390c9bbf5433cd0b5363
Craig Miller <craigmiller160@gmail.com>
2023-09-09 13:02:06 -0400

    Implemented a test

--- test/scripts/init/setupGitHooks.test.ts
+++ test/scripts/init/setupGitHooks.test.ts
@@ -1,7 +1,32 @@
-import { describe, it } from 'vitest';
+import { beforeEach, describe, expect, it, MockedFunction, vi } from 'vitest';
+import path from 'path';
+import { setupGitHooks } from '../../../scripts/init/setupGitHooks';
+import { runCommandSync } from '../../../scripts/utils/runCommand';
+import fs from 'fs';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'setupGitHooks'
+);
+
+const runCommandSyncMock = runCommandSync as MockedFunction<
+	typeof runCommandSync
+>;
 
 describe('setupGitHooks', () => {
-	it.fails('does nothing when no .git directory');
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
+
+	it('does nothing when no .git directory', () => {
+		const cwd = path.join(WORKING_DIR, 'noGit');
+		setupGitHooks(cwd, process);
+		const preCommitPath = path.join(cwd, '.husky', 'pre-commit');
+		expect(fs.existsSync(preCommitPath)).toBe(false);
+		expect(runCommandSyncMock).not.toHaveBeenCalled();
+	});
 	it.fails('aborts if .huksy directory not created');
 	it.fails('fully sets up git hooks');
 });
