3fb5c062eadf75a40edf5d4faff0078033cf4d74
Craig Miller <craigmiller160@gmail.com>
2023-09-09 18:06:09 -0400

    Finished vite setup

--- scripts/init/setupVite.ts
+++ scripts/init/setupVite.ts
@@ -3,8 +3,7 @@ import { PackageJson, PackageJsonType } from '../files/PackageJson';
 import { logger } from '../logger';
 import fs from 'fs';
 import path from 'path';
-import { match } from 'ts-pattern';
-import {vi} from 'vitest';
+import { unknownToError } from '../utils/unknownToError';
 
 export const VITE_CONFIG = `
 import { defineConfig } from '@craigmiller160/js-config/configs/vite/vite.config';
@@ -19,22 +18,50 @@ const getViteConfigPath = (cwd: string, type: PackageJsonType): string => {
 	return 'vite.config.ts';
 };
 
+type ExistingFileType = 'invalid' | 'valid' | 'none';
+
+const getExistingFileType = (
+	viteConfigPath: string
+): either.Either<Error, ExistingFileType> => {
+	if (!fs.existsSync(viteConfigPath)) {
+		return either.right('none');
+	}
+
+	return either.tryCatch((): ExistingFileType => {
+		const existingConfig = fs.readFileSync(viteConfigPath, 'utf8');
+		if (existingConfig.includes('@craigmiller160/js-config')) {
+			return 'valid';
+		}
+
+		return 'invalid';
+	}, unknownToError);
+};
+
 const backupExistingFile = (
 	cwd: string,
-	viteConfigPath: string
-): either.Either<Error, unknown> => {
-	if (!fs.existsSync(viteConfigPath)) {
-		return either.right(func.constVoid());
-	}
+	viteConfigPath: string,
+	existingFileType: ExistingFileType
+): either.Either<Error, unknown> =>
+	either.tryCatch(() => {
+		if (existingFileType === 'invalid') {
+			logger.debug('Backing up old vite config');
+			fs.renameSync(viteConfigPath, path.join(cwd, 'vite.config.backup'));
+		}
+	}, unknownToError);
 
-	const existingConfig = fs.readFileSync(viteConfigPath, 'utf8');
-	if (existingConfig.includes('@craigmiller160/js-config')) {
-		return either.right(func.constVoid());
-	}
-
-	logger.debug('Backing up old vite config');
-	fs.writeFileSync(path.join(cwd, 'vite.config.backup'), existingConfig);
-};
+const writeViteConfig = (
+	cwd: string,
+	viteConfigPath: string,
+	existingFileType: ExistingFileType
+): either.Either<Error, unknown> =>
+	either.tryCatch(() => {
+		if (existingFileType !== 'valid') {
+			logger.debug('Writing new vite config');
+			fs.writeFileSync(viteConfigPath, VITE_CONFIG.trim());
+		} else {
+			logger.debug('Valid vite config already exists');
+		}
+	}, unknownToError);
 
 export const setupVite = (
 	cwd: string,
@@ -45,6 +72,13 @@ export const setupVite = (
 		cwd,
 		packageJson.type ?? 'commonjs'
 	);
-	backupExistingFile(cwd, viteConfigPath);
-	throw new Error();
+	return func.pipe(
+		getExistingFileType(viteConfigPath),
+		either.chainFirst((existingFileType) =>
+			backupExistingFile(cwd, viteConfigPath, existingFileType)
+		),
+		either.chainFirst((existingFileType) =>
+			writeViteConfig(cwd, viteConfigPath, existingFileType)
+		)
+	);
 };
