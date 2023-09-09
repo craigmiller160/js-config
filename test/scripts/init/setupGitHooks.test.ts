ce4e0323132a7a46a2f8d4e077b729ecc73b1ab2
Craig Miller <craigmiller160@gmail.com>
2023-09-09 12:47:31 -0400

    stubbed out more tests

--- test/scripts/init/setupGitHooks.test.ts
+++ test/scripts/init/setupGitHooks.test.ts
@@ -1,5 +1,7 @@
 import { describe, it } from 'vitest';
 
 describe('setupGitHooks', () => {
-	it.fails('test');
+	it.fails('does nothing when no .git directory');
+	it.fails('aborts if .huksy directory not created');
+	it.fails('fully sets up git hooks');
 });
