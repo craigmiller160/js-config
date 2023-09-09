99b731ce9f0b31b0573c357c88ff735f0d22cdb8
Craig Miller <craigmiller160@gmail.com>
2023-09-09 13:59:35 -0400

    More test setup

--- test/scripts/init/setupVite.test.ts
+++ test/scripts/init/setupVite.test.ts
@@ -1,6 +1,33 @@
-import { describe, it } from 'vitest';
+import { afterEach, beforeEach, describe, it } from 'vitest';
+import path from 'path';
+import fs from 'fs';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'vite'
+);
+const cleanWorkingDir = () =>
+	fs
+		.readdirSync(WORKING_DIR)
+		.filter((fileName) => '.gitkeep' !== fileName)
+		.forEach((fileName) =>
+			fs.rmSync(path.join(WORKING_DIR, fileName), {
+				recursive: true,
+				force: true
+			})
+		);
 
 describe('setupVite', () => {
+	beforeEach(() => {
+		cleanWorkingDir();
+	});
+
+	afterEach(() => {
+		cleanWorkingDir();
+	});
+
 	it.fails('sets up vite config');
 
 	it.fails('backs up and replaces old vite config');
