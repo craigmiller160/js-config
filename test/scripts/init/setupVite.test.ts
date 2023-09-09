24ca71914c7f766686018ab7184f5cbef4b3b3ad
Craig Miller <craigmiller160@gmail.com>
2023-09-09 17:52:07 -0400

    Implemented another test

--- test/scripts/init/setupVite.test.ts
+++ test/scripts/init/setupVite.test.ts
@@ -2,7 +2,7 @@ import { afterEach, beforeEach, describe, it, expect } from 'vitest';
 import path from 'path';
 import fs from 'fs';
 import { PackageJson } from '../../../scripts/files/PackageJson';
-import {setupVite, VITE_CONFIG} from '../../../scripts/init/setupVite';
+import { setupVite, VITE_CONFIG } from '../../../scripts/init/setupVite';
 
 const WORKING_DIR = path.join(
 	process.cwd(),
@@ -49,7 +49,26 @@ describe('setupVite', () => {
 		expect(config).not.toMatch(/^.*foo.*$/);
 	});
 
-	it.fails('backs up and replaces old vite config');
+	it('backs up and replaces old vite config', () => {
+		fs.writeFileSync(VITE_CONFIG_PATH, 'Hello World');
+		const result = setupVite(WORKING_DIR, packageJson);
+		expect(result).toBeRight();
+
+		expect(fs.existsSync(VITE_CONFIG_PATH)).toBe(true);
+		const config = fs.readFileSync(VITE_CONFIG_PATH, 'utf8');
+		expect(config).toMatch(/^.*@craigmiller160\/js-config.*$/);
+		expect(config).not.toMatch(/^.*foo.*$/);
+
+		const backupConfigPath = path.join(
+			WORKING_DIR,
+			'vite.config.backup.ts'
+		);
+		expect(
+			fs.existsSync(path.join(WORKING_DIR, 'vite.config.backup.ts'))
+		).toBe(true);
+		const backupConfig = fs.readFileSync(backupConfigPath, 'utf8');
+		expect(backupConfig).toBe('Hello World');
+	});
 
 	it('does nothing when valid vite config is present', () => {
 		fs.writeFileSync(VITE_CONFIG_PATH, `${VITE_CONFIG.trim()}\n// foo`);
