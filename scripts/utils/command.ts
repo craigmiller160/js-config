17a1c82bd65f5120ca03764044c79ecabb79937f
Craig Miller <craigmiller160@gmail.com>
2023-09-09 11:13:35 -0400

    Avoided errors

--- scripts/utils/command.ts
+++ scripts/utils/command.ts
@@ -2,11 +2,15 @@ import { either } from 'fp-ts';
 import path from 'path';
 import fs from 'fs';
 
+const getNodePath = (process: NodeJS.Process): string =>
+	process.env.NODE_PATH ?? path.join(process.cwd(), 'node_modules');
+
 export const findCommand = (
 	process: NodeJS.Process,
 	pathFromNodeModules: string
 ): either.Either<Error, string> => {
-	const matchingPaths = process.env.NODE_PATH.split(':')
+	const matchingPaths = getNodePath(process)
+		.split(':')
 		.map((thePath) => path.join(thePath, pathFromNodeModules))
 		.filter((theFullPath) => fs.existsSync(theFullPath));
 
