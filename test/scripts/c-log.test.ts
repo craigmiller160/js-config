1fe67831643c5696ff73ed2c203fd5c9a16ca756
Craig Miller <craigmiller160@gmail.com>
2023-09-10 09:45:07 -0400

    Implemented a new test

--- test/scripts/c-log.test.ts
+++ test/scripts/c-log.test.ts
@@ -1,5 +1,25 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import path from 'path';
+import { execute, LogToStdout } from '../../scripts/c-log';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'log',
+	'child'
+);
 
 describe('c-log', () => {
-	it.fails('reads last logs');
+	it('reads last logs', () => {
+		let receivedLogText: string = '';
+		const logToStdout: LogToStdout = (logText) => {
+			receivedLogText = logText;
+		};
+
+		execute(logToStdout, WORKING_DIR);
+		expect(receivedLogText).toBe(
+			'This is a sample log\nJust including all the data'
+		);
+	});
 });
