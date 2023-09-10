dc2203d675b3b89956302d2e578b41e8bc5b5fe7
Craig Miller <craigmiller160@gmail.com>
2023-09-10 10:06:53 -0400

    Refactored script to setup git hooks

--- scripts/commandPaths.ts
+++ scripts/commandPaths.ts
@@ -3,3 +3,4 @@ export const SWC = '@swc/cli/bin/swc.js';
 export const ESLINT = 'eslint/bin/eslint.js';
 export const HUSKY = 'husky/lib/bin.js';
 export const VITEST = 'vitest/vitest.mjs';
+export const LINT_STAGED = 'lint-staged/bin/lint-staged.js';
