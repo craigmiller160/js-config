ff6b0d4ac56585f568f417d716ccac12840b4936
Craig Miller <craigmiller160@gmail.com>
2023-09-10 14:28:10 -0400

    Cleanup

--- scripts/utils/terminate.ts
+++ scripts/utils/terminate.ts
@@ -3,6 +3,6 @@ import { logger } from '../logger';
 export const terminate = (result: Error | unknown): void => {
 	if (result instanceof Error) {
 		logger.error(result);
-		setImmediate(() => process.exit(1));
+		process.exitCode = 1;
 	}
 };
