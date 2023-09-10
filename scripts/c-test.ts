a1c1ba6abd08771e1fbd3f584ad6321f7a95ce69
Craig Miller <craigmiller160@gmail.com>
2023-09-10 09:46:40 -0400

    Working on test script

--- scripts/c-test.ts
+++ scripts/c-test.ts
@@ -1,1 +1,6 @@
-export const execute = () => {};
+import { runCommandSync } from './utils/runCommand';
+import { function as func, either } from 'fp-ts';
+import { terminate } from './utils/terminate';
+
+export const execute = () =>
+	func.pipe(runCommandSync('vitest run'), either.fold(terminate, terminate));
