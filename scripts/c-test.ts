d88db0b25b13048b4d557a9a8d7c55dd332b6f06
Craig Miller <craigmiller160@gmail.com>
2023-09-10 09:47:55 -0400

    Implemented script

--- scripts/c-test.ts
+++ scripts/c-test.ts
@@ -1,6 +1,11 @@
 import { runCommandSync } from './utils/runCommand';
-import { function as func, either } from 'fp-ts';
+import { either, function as func } from 'fp-ts';
 import { terminate } from './utils/terminate';
+import { findCommand } from './utils/command';
 
-export const execute = () =>
-	func.pipe(runCommandSync('vitest run'), either.fold(terminate, terminate));
+export const execute = (process: NodeJS.Process) =>
+	func.pipe(
+		findCommand(process, 'vitest/vitest.mjs'),
+		either.chain((command) => runCommandSync(`${command} run`)),
+		either.fold(terminate, terminate)
+	);
