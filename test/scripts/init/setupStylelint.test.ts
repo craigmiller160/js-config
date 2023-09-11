39310a908a820f1e65ef129c2ede3f05d8ae6791
Craig Miller <craigmiller160@gmail.com>
2023-09-11 07:58:38 -0400

    Test setup logic

--- test/scripts/init/setupStylelint.test.ts
+++ test/scripts/init/setupStylelint.test.ts
@@ -1,6 +1,32 @@
-import { describe, it } from 'vitest';
+import { afterEach, beforeEach, describe, it } from 'vitest';
+import path from 'path';
+import fs from 'fs';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'stylelint'
+);
+const clearDirectory = () =>
+	fs
+		.readdirSync(WORKING_DIR)
+		.filter((fileName) => '.gitkeep' !== fileName)
+		.map((fileName) => path.join(WORKING_DIR, fileName))
+		.forEach((filePath) =>
+			fs.rmSync(filePath, {
+				recursive: true,
+				force: true
+			})
+		);
 
 describe('setupStylelint', () => {
+	beforeEach(() => {
+		clearDirectory();
+	});
+	afterEach(() => {
+		clearDirectory();
+	});
 	it.fails('sets up stylelint config file');
 
 	it.fails('replaces existing config file if invalid');
