c0246162b8587f5454ab00db8eb40586aae023a0
Craig Miller <craigmiller160@gmail.com>
2023-09-10 21:49:45 -0400

    Fully setup stylelint

--- scripts/init/setupStylelint.ts
+++ scripts/init/setupStylelint.ts
@@ -1,9 +1,42 @@
-import { either } from 'fp-ts';
+import { either, function as func } from 'fp-ts';
+import { parseStylelintrc, Stylelintrc } from '../files/Stylelintrc';
+import path from 'path';
+import fs from 'fs';
+import { logger } from '../logger';
+import { unknownToError } from '../utils/unknownToError';
 
-type StylelintConfig = Readonly<{
-	extends: string;
-}>;
+const DEFAULT_CONFIG: Stylelintrc = {
+	extends: '@craigmiller160/js-config/configs/stylelint/.stylelintrc.json'
+};
 
-export const setupStylelint = (
-	cwd: string
-): either.Either<Error, unknown> => {};
+const getStylelintrcPath = (cwd: string): string =>
+	path.join(cwd, '.stylelintrc.json');
+
+const shouldWriteStylelintrc = (cwd: string) => {
+	const stylelintrcPath = getStylelintrcPath(cwd);
+	if (fs.existsSync(stylelintrcPath)) {
+		return func.pipe(
+			parseStylelintrc(stylelintrcPath),
+			either.map(
+				(stylelintrc) =>
+					!stylelintrc.extends.includes('@craigmiller160/js-config')
+			),
+			either.fold(() => true, func.identity)
+		);
+	}
+	return true;
+};
+
+export const setupStylelint = (cwd: string): either.Either<Error, unknown> => {
+	logger.info('Setting up stylelint');
+	if (!shouldWriteStylelintrc(cwd)) {
+		logger.debug('Stylelint already configured');
+		return either.right(func.constVoid());
+	}
+
+	const stylelintrcPath = getStylelintrcPath(cwd);
+	return either.tryCatch(
+		() => fs.writeFileSync(stylelintrcPath, JSON.stringify(DEFAULT_CONFIG)),
+		unknownToError
+	);
+};
