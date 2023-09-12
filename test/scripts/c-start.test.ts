09a4e8bc8111d2b618d8888f20d198ffaed5e002
Craig Miller <craigmiller160@gmail.com>
2023-09-12 13:40:14 -0400

    Implemented another test

--- test/scripts/c-start.test.ts
+++ test/scripts/c-start.test.ts
@@ -1,5 +1,19 @@
-import { describe, it } from 'vitest';
+import { beforeEach, describe, it, MockedFunction, vi, expect } from 'vitest';
+import { runCommandSync } from '../../scripts/utils/runCommand';
+import { execute } from '../../scripts/c-start';
+import { either } from 'fp-ts';
+
+const runCommandSyncMock = runCommandSync as MockedFunction<
+	typeof runCommandSync
+>;
 
 describe('c-start', () => {
-	it.fails('starts vite dev server');
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
+	it('starts vite dev server', () => {
+		runCommandSyncMock.mockReturnValue(either.right(''));
+		execute();
+		expect(runCommandSyncMock).toHaveBeenCalledWith('vite start');
+	});
 });
