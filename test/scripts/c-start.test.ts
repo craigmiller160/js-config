f1902fb4bdf375c234f7f8e5aa748368c37bf44c
Craig Miller <craigmiller160@gmail.com>
2023-09-12 14:01:45 -0400

    Fixed test

--- test/scripts/c-start.test.ts
+++ test/scripts/c-start.test.ts
@@ -2,10 +2,15 @@ import { beforeEach, describe, expect, it, MockedFunction, vi } from 'vitest';
 import { runCommandAsync } from '../../scripts/utils/runCommand';
 import { execute } from '../../scripts/c-start';
 import { taskEither } from 'fp-ts';
+import path from 'path';
+import { TSC, VITE } from '../../scripts/commandPaths';
 
 const runCommandAsyncMock = runCommandAsync as MockedFunction<
 	typeof runCommandAsync
 >;
+
+const VITE_CMD = path.join(process.cwd(), 'node_modules', VITE);
+const TSC_CMD = path.join(process.cwd(), 'node_modules', TSC);
 
 describe('c-start', () => {
 	beforeEach(() => {
@@ -14,10 +19,10 @@ describe('c-start', () => {
 
 	it('starts vite dev server', () => {
 		runCommandAsyncMock.mockReturnValue(taskEither.right(''));
-		execute();
-		expect(runCommandAsyncMock).toHaveBeenCalledWith('vite start');
+		execute(process);
+		expect(runCommandAsyncMock).toHaveBeenCalledWith(`${VITE_CMD} start`);
 		expect(runCommandAsyncMock).toHaveBeenCalledWith(
-			'tsc --noEmit --watch'
+			`${TSC_CMD} --noEmit --watch`
 		);
 	});
 });
