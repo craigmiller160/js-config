a930cec171bc716306899a88b00a89d610c180a6
Craig Miller <craigmiller160@gmail.com>
2023-09-12 13:50:07 -0400

    Added runCommandAsync

--- scripts/utils/runCommand.ts
+++ scripts/utils/runCommand.ts
@@ -1,6 +1,57 @@
-import { spawnSync, SpawnOptions } from 'child_process';
-import { either } from 'fp-ts';
+import { spawnSync, SpawnOptions, spawn } from 'child_process';
+import { either, taskEither } from 'fp-ts';
 import { logger } from '../logger';
+import { unknownToError } from './unknownToError';
+
+export const runCommandAsync = (
+	command: string,
+	options?: SpawnOptions
+): taskEither.TaskEither<Error, string> => {
+	logger.debug(`Running command: ${command}`);
+	const commandParts = command.split(' ');
+
+	return taskEither.tryCatch(
+		() =>
+			new Promise((resolve, reject) => {
+				const childProcess = spawn(
+					commandParts[0],
+					commandParts.slice(1),
+					{
+						...(options ?? {}),
+						stdio: 'pipe'
+					}
+				);
+
+				let output = '';
+				let error = '';
+
+				childProcess.stdout.on('data', (data: Buffer) => {
+					const text = data.toString('utf8');
+					output += `${text}\n`;
+					logger.debug(`  STDOUT: ${output}`);
+				});
+
+				childProcess.stderr.on('data', (data: Buffer) => {
+					const text = data.toString('utf8');
+					error += `${text}\n`;
+					logger.debug(`  STDERR: ${error}`);
+				});
+
+				childProcess.on('exit', (code) => {
+					if (code === 0) {
+						resolve(output);
+					} else {
+						reject(
+							new Error(
+								`Command failed. Status: ${code} Message: ${error}`
+							)
+						);
+					}
+				});
+			}),
+		unknownToError
+	);
+};
 
 export const runCommandSync = (
 	command: string,
