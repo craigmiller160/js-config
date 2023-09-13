2dd88cede945afe1b2b375bcb92d680cf0c3787d
Craig Miller <craigmiller160@gmail.com>
2023-09-13 17:42:31 -0400

    Added comment

--- scripts/c-cypress.ts
+++ scripts/c-cypress.ts
@@ -1,5 +1,6 @@
 import { logger } from './logger';
 
+// cypress run --component -b electron
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Running all cypress tests');
 	throw new Error();
