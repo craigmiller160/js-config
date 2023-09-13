ecea42187b754862aca94226b847f9bd5d470932
Craig Miller <craigmiller160@gmail.com>
2023-09-13 16:51:12 -0400

    Implemented another test

--- test/scripts/c-build-app.test.ts
+++ test/scripts/c-build-app.test.ts
@@ -1,5 +1,23 @@
-import { describe, it } from 'vitest';
+import { beforeEach, describe, it, MockedFunction, vi, expect } from 'vitest';
+import { runCommandSync } from '../../scripts/utils/runCommand';
+import { either } from 'fp-ts';
+import { execute } from '../../scripts/c-build-app';
+import path from 'path';
+import { VITE } from '../../scripts/commandPaths';
+
+const runCommandSyncMock = runCommandSync as MockedFunction<
+	typeof runCommandSync
+>;
+const COMMAND = path.join(process.cwd(), 'node_modules', VITE);
 
 describe('c-build-app', () => {
-	it.fails('runs vite build');
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
+	it('runs vite build', () => {
+		runCommandSyncMock.mockReturnValue(either.right(''));
+		execute(process);
+
+		expect(runCommandSyncMock).toHaveBeenCalledWith(`${COMMAND} build`);
+	});
 });
