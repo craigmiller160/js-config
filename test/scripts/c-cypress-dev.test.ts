006a2011e1f558ba467017eaaf27d1594e5d8793
Craig Miller <craigmiller160@gmail.com>
2023-09-14 12:51:31 -0400

    Test setup logic

--- test/scripts/c-cypress-dev.test.ts
+++ test/scripts/c-cypress-dev.test.ts
@@ -1,5 +1,17 @@
-import { describe, it } from 'vitest';
+import {beforeEach, describe, it, MockedFunction, vi} from 'vitest';
+import path from 'path';
+import { CYPRESS } from '../../scripts/commandPaths';
+import { runCommandSync } from '../../scripts/utils/runCommand';
+
+const runCommandSyncMock = runCommandSync as MockedFunction<
+	typeof runCommandSync
+>;
+
+const COMMAND = path.join(process.cwd(), 'node_modules', CYPRESS);
 
 describe('c-cypress-dev', () => {
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
 	it.fails('runs command');
 });
--- test/scripts/c-cypress.test.ts
+++ test/scripts/c-cypress.test.ts
@@ -1,5 +1,17 @@
-import { describe, it } from 'vitest';
+import {beforeEach, describe, it, MockedFunction, vi} from 'vitest';
+import { runCommandSync } from '../../scripts/utils/runCommand';
+import path from 'path';
+import { CYPRESS } from '../../scripts/commandPaths';
+
+const runCommandSyncMock = runCommandSync as MockedFunction<
+	typeof runCommandSync
+>;
+
+const COMMAND = path.join(process.cwd(), 'node_modules', CYPRESS);
 
 describe('c-cypress', () => {
+	beforeEach(() => {
+		vi.resetAllMocks();
+	});
 	it.fails('runs command');
 });
