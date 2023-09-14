748d0c369d65c6a86ee5f58f5244632aadddce4c
Craig Miller <craigmiller160@gmail.com>
2023-09-14 12:57:46 -0400

    Finished command

--- scripts/c-cypress.ts
+++ scripts/c-cypress.ts
@@ -1,7 +1,17 @@
 import { logger } from './logger';
+import { either, function as func } from 'fp-ts';
+import { findCommand } from './utils/command';
+import { CYPRESS } from './commandPaths';
+import { runCommandSync } from './utils/runCommand';
+import { terminate } from './utils/terminate';
 
-// cypress run --component -b electron
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Running all cypress tests');
-	throw new Error();
+	func.pipe(
+		findCommand(process, CYPRESS),
+		either.chain((command) =>
+			runCommandSync(`${command} run --component -b electron`)
+		),
+		either.fold(terminate, terminate)
+	);
 };
