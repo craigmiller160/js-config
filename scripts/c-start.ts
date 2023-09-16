fafb70ea0594a574b94aa8378529a1835c6b3138
Craig Miller <craigmiller160@gmail.com>
2023-09-16 14:03:05 -0400

    Updated script

--- scripts/c-start.ts
+++ scripts/c-start.ts
@@ -4,16 +4,19 @@ import { findCommand } from './utils/command';
 import { TSC, VITE } from './commandPaths';
 import { either, function as func } from 'fp-ts';
 import { terminate } from './utils/terminate';
+import { getRealArgs } from './utils/process';
 
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Starting dev server');
+
+	const args = getRealArgs(process).join(' ');
 
 	func.pipe(
 		findCommand(process, VITE),
 		either.bindTo('viteCommand'),
 		either.bind('tscCommand', () => findCommand(process, TSC)),
 		either.fold(terminate, ({ viteCommand, tscCommand }) => {
-			runCommandAsync(`${viteCommand} start`)();
+			runCommandAsync(`${viteCommand} start ${args}`)();
 			runCommandAsync(`${tscCommand} --noEmit --watch`)();
 		})
 	);
