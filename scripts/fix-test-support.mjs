49d02708acd1c103bca5c50a7084da94ebd76ea2
Craig Miller <craigmiller160@gmail.com>
2023-09-18 07:25:49 -0600

    Writing script to fix output

---
+++ scripts/fix-test-support.mjs
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+const TEST_SUPPORT_BUILD_PATH = path.join(
+	__dirname,
+	'..',
+	'build',
+	'test-support'
+);
+
+fs.readdirSync(TEST_SUPPORT_BUILD_PATH)
+	.filter((fileName) => path.extname(fileName) === '.js')
+	.forEach((fileName) => {
+		const oldPath = path.join(TEST_SUPPORT_BUILD_PATH, fileName);
+		const newFileName = `${path.parse(fileName).name}.mjs`;
+		const newPath = path.join(TEST_SUPPORT_BUILD_PATH, newFileName);
+		fs.renameSync(oldPath, newPath);
+	});
