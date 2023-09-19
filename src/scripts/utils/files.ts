f08bc55e69830f293a24071e805239ddd1fddcf0
Craig Miller <craigmiller160@gmail.com>
2023-09-18 18:49:57 -0600

    Wrote file walking function

---
+++ src/scripts/utils/files.ts
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const isDirectory = async (filePath: string): Promise<boolean> => {
+	const stats = await fs.stat(filePath);
+	return stats.isDirectory();
+};
+
+export const walk = async (
+	filePath: string
+): Promise<ReadonlyArray<string>> => {
+	const filePathIsDirectory = await isDirectory(filePath);
+	if (!filePathIsDirectory) {
+		return [filePath];
+	}
+
+	const files = await fs.readdir(filePath);
+	const promises = files.map((file) => walk(path.join(filePath, file)));
+	const walkResults = await Promise.all(promises);
+	return walkResults.flat();
+};
