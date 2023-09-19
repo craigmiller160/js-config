38f321804ce69f9bbaabcf0459a6d318dc352fc2
Craig Miller <craigmiller160@gmail.com>
2023-09-18 18:53:38 -0600

    Test passes

--- test/scripts/utils/files.test.ts
+++ test/scripts/utils/files.test.ts
@@ -14,14 +14,14 @@ describe('files', () => {
 		it('walks file tree', async () => {
 			const results = await walk(WORKING_DIR);
 			expect(results).toEqual([
+				path.join(WORKING_DIR, 'child1', 'child1.txt'),
+				path.join(WORKING_DIR, 'child1', 'child1b.txt'),
 				path.join(
 					WORKING_DIR,
 					'child1',
 					'grandchild1',
 					'grandchild1.txt'
 				),
-				path.join(WORKING_DIR, 'child1', 'child1.txt'),
-				path.join(WORKING_DIR, 'child1', 'child1b.txt'),
 				path.join(WORKING_DIR, 'child2', 'child2.txt'),
 				path.join(WORKING_DIR, 'root.txt')
 			]);
