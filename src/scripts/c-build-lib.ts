673b856e4de3f83a652482e63fb9330a14eec6c1
Craig Miller <craigmiller160@gmail.com>
2023-09-18 19:01:08 -0600

    Implementing new function

--- src/scripts/c-build-lib.ts
+++ src/scripts/c-build-lib.ts
@@ -1,59 +1,48 @@
 import path from 'path';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { logger } from './logger';
 import { runCommandSync } from './utils/runCommand';
-import { function as func, either } from 'fp-ts';
+import { function as func, either, taskEither } from 'fp-ts';
 import { terminate } from './utils/terminate';
 import { findCommand } from './utils/command';
 import { SWC, TSC } from './commandPaths';
+import { transformFile } from '@swc/core';
+import { unknownToError } from './utils/unknownToError';
+import { match } from 'ts-pattern';
+
+type CompileType = 'ecmascript' | 'typescript';
+type ModuleType = 'es6' | 'commonjs';
+
+const SWCRC_CONFIG_DIR = path.join(__dirname, '..', '..', 'configs', 'swc');
+const SWCRC_JS = path.join(SWCRC_CONFIG_DIR, '.swcrc_js');
+const SWCRC_TS = path.join(SWCRC_CONFIG_DIR, '.swcrc_ts');
+
+const getSwcConfigPath = (compileType: CompileType): string =>
+	match(compileType)
+		.with('ecmascript', () => SWCRC_JS)
+		.with('typescript', () => SWCRC_TS)
+		.exhaustive();
+
+const createCompile =
+	(srcDir: string, destDir: string) =>
+	(file: string, compileType: CompileType, moduleType: ModuleType) => {
+		const configFile = getSwcConfigPath(compileType);
+		func.pipe(
+			taskEither.tryCatch(
+				() =>
+					transformFile(file, {
+						configFile,
+						module: {
+							type: moduleType
+						}
+					}),
+				unknownToError
+			)
+		);
+	};
 
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Performing library build');
 	const srcDir = path.join(process.cwd(), 'src');
-	const libDir = path.join(process.cwd(), 'lib');
-	if (fs.existsSync(libDir)) {
-		fs.rmSync(libDir, {
-			recursive: true,
-			force: true
-		});
-	}
-
-	const esModuleDir = path.join(libDir, 'esm');
-	const commonjsDir = path.join(libDir, 'cjs');
-	const typesDir = path.join(libDir, 'types');
-	const configRoot = path.join(__dirname, '..', '..', 'configs', 'swc');
-	const jsConfigPath = path.join(configRoot, '.swcrc_js');
-	const tsConfigPath = path.join(configRoot, '.swcrc_ts');
-
-	func.pipe(
-		findCommand(process, SWC),
-		either.bindTo('swcCommand'),
-		either.bind('tscCommand', () => findCommand(process, TSC)),
-		either.chainFirst(({ swcCommand }) =>
-			runCommandSync(
-				`${swcCommand} ${srcDir} -d ${esModuleDir} --config-file ${jsConfigPath} -C module.type=es6 --only **/*.{js,mjs,cjs,jsx}`
-			)
-		),
-		either.chainFirst(({ swcCommand }) =>
-			runCommandSync(
-				`${swcCommand} ${srcDir} -d ${esModuleDir} --config-file ${tsConfigPath} -C module.type=es6 --only **/*.{ts,mts,cts,tsx}`
-			)
-		),
-		either.chainFirst(({ swcCommand }) =>
-			runCommandSync(
-				`${swcCommand} ${srcDir} -d ${commonjsDir} --config-file ${jsConfigPath} -C module.type=commonjs --only **/*.{js,mjs,cjs,jsx}`
-			)
-		),
-		either.chainFirst(({ swcCommand }) =>
-			runCommandSync(
-				`${swcCommand} ${srcDir} -d ${commonjsDir} --config-file ${tsConfigPath} -C module.type=commonjs --only **/*.{ts,mts,cts,tsx}`
-			)
-		),
-		either.chainFirst(({ tscCommand }) =>
-			runCommandSync(
-				`${tscCommand} --declaration --emitDeclarationOnly --outDir ${typesDir}`
-			)
-		),
-		either.fold(terminate, terminate)
-	);
+	const destDir = path.join(process.cwd(), 'lib');
 };
