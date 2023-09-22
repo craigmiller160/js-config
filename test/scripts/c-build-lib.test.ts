a83b6ec4ea10bab55f19b9e379e3e0cf2ed54dc8
Craig Miller <craigmiller160@gmail.com>
2023-09-21 20:29:53 -0400

    Stubbing out tests

--- test/scripts/c-build-lib.test.ts
+++ test/scripts/c-build-lib.test.ts
@@ -5,38 +5,26 @@ import path from 'path';
 import { either } from 'fp-ts';
 import { SWC, TSC } from '../../src/scripts/commandPaths';
 
+const WORKING_DIR = path.join(process.cwd(), 'test', '__working_directories', 'buildLib');
 const runCommandSyncMock = runCommandSync as MockedFunction<
 	typeof runCommandSync
 >;
-const srcDir = path.join(process.cwd(), 'src');
-const libDir = path.join(process.cwd(), 'lib');
+const srcDir = path.join(WORKING_DIR, 'src');
+const libDir = path.join(WORKING_DIR, 'lib');
 const esModuleDir = path.join(libDir, 'esm');
 const commonjsDir = path.join(libDir, 'cjs');
 const typesDir = path.join(libDir, 'types');
-const configPath = path.join(process.cwd(), 'configs', 'swc', '.swcrc');
 
-const swcCommand = path.join(process.cwd(), 'node_modules', SWC);
 const tscCommand = path.join(process.cwd(), 'node_modules', TSC);
 
 describe('c-build-lib', () => {
 	beforeEach(() => {
 		vi.resetAllMocks();
 	});
-	it('runs build', () => {
-		runCommandSyncMock.mockReturnValue(either.right(''));
-		execute(process);
-		expect(runCommandSyncMock).toHaveBeenCalledTimes(3);
-		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
-			1,
-			`${swcCommand} ${srcDir} -d ${esModuleDir} --config-file ${configPath} -C module.type=es6`
-		);
-		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
-			2,
-			`${swcCommand} ${srcDir} -d ${commonjsDir} --config-file ${configPath} -C module.type=commonjs`
-		);
-		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
-			3,
-			`${tscCommand} --declaration --emitDeclarationOnly --outDir ${typesDir}`
-		);
-	});
+
+	it.fails('performs the entire library build for both esm and cjs');
+
+	it.fails('performs the entire library build for just esm');
+
+	it.fails('performs the entire library build for just cjs');
 });
