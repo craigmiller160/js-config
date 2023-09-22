aa51956afb35c13049111a54488a76c245cc85f1
Craig Miller <craigmiller160@gmail.com>
2023-09-21 20:31:03 -0400

    More test setup logic

--- test/scripts/c-build-lib.test.ts
+++ test/scripts/c-build-lib.test.ts
@@ -4,11 +4,14 @@ import { runCommandSync } from '../../src/scripts/utils/runCommand';
 import path from 'path';
 import { either } from 'fp-ts';
 import { SWC, TSC } from '../../src/scripts/commandPaths';
+import fs from 'fs';
 
-const WORKING_DIR = path.join(process.cwd(), 'test', '__working_directories', 'buildLib');
-const runCommandSyncMock = runCommandSync as MockedFunction<
-	typeof runCommandSync
->;
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories',
+	'buildLib'
+);
 const srcDir = path.join(WORKING_DIR, 'src');
 const libDir = path.join(WORKING_DIR, 'lib');
 const esModuleDir = path.join(libDir, 'esm');
@@ -17,9 +20,15 @@ const typesDir = path.join(libDir, 'types');
 
 const tscCommand = path.join(process.cwd(), 'node_modules', TSC);
 
+vi.unmock('../../src/scripts/utils/runCommand');
+
 describe('c-build-lib', () => {
 	beforeEach(() => {
 		vi.resetAllMocks();
+		fs.rmSync(libDir, {
+			recursive: true,
+			force: true
+		});
 	});
 
 	it.fails('performs the entire library build for both esm and cjs');
