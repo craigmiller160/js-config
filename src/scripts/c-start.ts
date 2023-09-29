b9850ed8b60e308fca2815ff1f203faa394354c8
Craig Miller <craigmiller160@gmail.com>
2023-09-29 16:55:44 -0400

    Improved start script

--- src/scripts/c-start.ts
+++ src/scripts/c-start.ts
@@ -5,18 +5,20 @@ import { TSC, VITE } from './commandPaths';
 import { either, function as func } from 'fp-ts';
 import { terminate } from './utils/terminate';
 import { getRealArgs } from './utils/process';
+import path from 'path';
 
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Starting dev server');
 
 	const args = getRealArgs(process).join(' ');
+	const config = path.join(process.cwd(), 'vite.config.mts');
 
 	func.pipe(
 		findCommand(process, VITE),
 		either.bindTo('viteCommand'),
 		either.bind('tscCommand', () => findCommand(process, TSC)),
 		either.fold(terminate, ({ viteCommand, tscCommand }) => {
-			runCommandAsync(`${viteCommand} start ${args}`)();
+			runCommandAsync(`${viteCommand} start ${args} -c ${config}`)();
 			runCommandAsync(`${tscCommand} --noEmit --watch`)();
 		})
 	);
