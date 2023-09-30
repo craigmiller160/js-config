1ed09fd0491a53c8371f2bf673b6ebc71ec3538b
Craig Miller <craigmiller160@gmail.com>
2023-09-30 15:30:33 -0400

    Updating tests

--- test/scripts/c-eslint.test.ts
+++ test/scripts/c-eslint.test.ts
@@ -16,27 +16,68 @@ const COMMAND = path.join(
 	'eslint.js'
 );
 
-describe('c-lint', () => {
+describe('c-eslint', () => {
 	beforeEach(() => {
 		vi.resetAllMocks();
 		runCommandSyncMock.mockReturnValue(either.right(''));
 	});
 
-	it('runs with default path', () => {
+	it('runs with default path and no cypress', () => {
 		execute(process);
-		expect(runCommandSync).toHaveBeenCalledWith(
-			`${COMMAND} --fix --max-warnings=0 {src,test,cypress}/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}`
+		expect(runCommandSyncMock).toHaveBeenCalledTimes(1);
+		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
+			1,
+			`${COMMAND} --fix --max-warnings=0 {src,test}/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}`
 		);
 	});
 
-	it('runs with explicit path', () => {
+	it('runs with default path and cypress', () => {
+		execute(process);
+		expect(runCommandSyncMock).toHaveBeenCalledTimes(2);
+		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
+			1,
+			`${COMMAND} --fix --max-warnings=0 {src,test}/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}`
+		);
+		expect(runCommandSyncMock).toHaveBeenNthCalledWith(
+			2,
+			`${COMMAND} --fix --max-warnings=0 cypress/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}`,
+			{
+				env: expect.objectContaining({
+					NO_VITEST: 'true'
+				})
+			}
+		);
+	});
+
+	it('runs with explicit path and not a cypress path', () => {
 		const thePath = 'foo/bar.js';
 		execute({
 			...process,
 			argv: ['', '', thePath]
 		});
-		expect(runCommandSync).toHaveBeenCalledWith(
-			`${COMMAND} --fix --max-warnings=0 ${thePath}`
+		expect(runCommandSyncMock).toHaveBeenCalledWith(
+			`${COMMAND} --fix --max-warnings=0 ${thePath}`,
+			{
+				env: expect.objectContaining({
+					NO_VITEST: 'false'
+				})
+			}
+		);
+	});
+
+	it('runs with explicit path that is a cypress path', () => {
+		const thePath = 'foo/bar.js';
+		execute({
+			...process,
+			argv: ['', '', thePath]
+		});
+		expect(runCommandSyncMock).toHaveBeenCalledWith(
+			`${COMMAND} --fix --max-warnings=0 ${thePath}`,
+			{
+				env: expect.objectContaining({
+					NO_VITEST: 'true'
+				})
+			}
 		);
 	});
 });
