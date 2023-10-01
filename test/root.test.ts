182426d29db3448b57aa57b0c70736283405070b
Craig Miller <craigmiller160@gmail.com>
2023-10-01 14:59:32 -0400

    Stubbing out tests

---
+++ test/root.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+
+describe('root', () => {
+	describe('getProjectRoot', () => {
+		it.fails('is source path');
+
+		it.fails('is build path');
+	});
+});
