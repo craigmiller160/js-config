843a05235dbe93669dc81330c85b7741256213c0
Craig Miller <craigmiller160@gmail.com>
2023-10-01 15:05:38 -0400

    Small fix

--- src/root.ts
+++ src/root.ts
@@ -2,8 +2,9 @@ import path from 'path';
 import fs from 'fs';
 
 export const getProjectRoot = (currentDir: string = __dirname): string => {
-	const rootSrcPath = path.join(currentDir, '..', '..');
+	const rootSrcPath = path.join(currentDir, '..');
 	const rootBuildPath = path.join(currentDir, '..', '..', '..');
+    console.log(rootSrcPath);
 	if (fs.existsSync(path.join(rootSrcPath, 'src'))) {
 		return rootSrcPath;
 	}
