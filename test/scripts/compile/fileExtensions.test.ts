3951979215b3e0fd1a31ad6098076e6b5ee56d0a
Craig Miller <craigmiller160@gmail.com>
2023-10-01 15:20:56 -0400

    wrote a lot of test setup logic

--- test/scripts/compile/fileExtensions.test.ts
+++ test/scripts/compile/fileExtensions.test.ts
@@ -1,6 +1,33 @@
-import { describe, it } from 'vitest';
+import { beforeEach, describe, it } from 'vitest';
+import path from 'path';
+import fs from 'fs/promises';
+
+const TYPE_EXTENSION_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'fileExtensions',
+	'types'
+);
 
 describe('compile file extension utilities', () => {
+	beforeEach(async () => {
+		await fs
+			.readdir(TYPE_EXTENSION_DIR)
+			.then((files) =>
+				files
+					.filter((file) => '.gitkeep' !== file)
+					.map((file) => path.join(TYPE_EXTENSION_DIR, file))
+					.map((file) => fs.rm(file))
+			)
+			.then((promises) => Promise.all(promises));
+		await Promise.all([
+			fs.writeFile(path.join(TYPE_EXTENSION_DIR, 'file.d.ts'), ''),
+			fs.writeFile(path.join(TYPE_EXTENSION_DIR, 'file.d.mts'), ''),
+			fs.writeFile(path.join(TYPE_EXTENSION_DIR, 'file.d.cts'), '')
+		]);
+	});
+
 	describe('fixFileExtension', () => {
 		it.fails('.d.ts');
 		it.fails('.d.mts');
