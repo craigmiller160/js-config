c85267390467456ee59bc4b128ca8fe508c030cf
Craig Miller <craigmiller160@gmail.com>
2023-10-02 22:41:05 -0400

    Working on tests

--- test/scripts/compile/compile.test.ts
+++ test/scripts/compile/compile.test.ts
@@ -1,6 +1,7 @@
-import { beforeEach, describe, it } from 'vitest';
+import { beforeEach, describe, it, expect } from 'vitest';
 import path from 'path';
 import fs from 'fs/promises';
+import { createCompile } from '../../../src/scripts/compile';
 
 const WORKING_DIR = path.join(
 	process.cwd(),
@@ -9,6 +10,13 @@ const WORKING_DIR = path.join(
 	'compile'
 );
 const OUT_DIR = path.join(WORKING_DIR, 'out');
+const TS_INPUT_FILE = path.join(WORKING_DIR, 'example-ts.ts');
+const TS_OUTPUT_FILE = path.join(OUT_DIR, 'example-ts.js');
+const JS_INPUT_FILE = path.join(WORKING_DIR, 'example-js.js');
+const JS_OUTPUT_FILE = path.join(OUT_DIR, 'example-js.js');
+
+const fileExists = (file: string): Promise<boolean> =>
+	fs.stat(file).then((stats) => stats.isFile());
 
 describe('compile file', () => {
 	beforeEach(async () => {
@@ -22,7 +30,18 @@ describe('compile file', () => {
 			.then((promises) => Promise.all(promises));
 	});
 
-	it.fails('compiles ts file with esmodules');
+	it('compiles ts file with esmodules', async () => {
+		const existsBefore = await fileExists(TS_OUTPUT_FILE);
+		expect(existsBefore).toBe(false);
+		await createCompile(WORKING_DIR, OUT_DIR, 'es6')(TS_INPUT_FILE)();
+
+		const existsAfter = await fileExists(TS_OUTPUT_FILE);
+		expect(existsAfter).toBe(true);
+		const content = await fs.readFile(TS_OUTPUT_FILE, 'utf8');
+		expect(content).toBe(
+			`/* eslint-disable */ export const hello = 'world';\n`
+		);
+	});
 	it.fails('compiles ts file with commonjs');
 	it.fails('compiles js file with esmodules');
 	it.fails('compiles js file with commonjs');
