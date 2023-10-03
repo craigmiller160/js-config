073a44feb5cb7914b7aade8479ac65eee2f0f06d
Craig Miller <craigmiller160@gmail.com>
2023-10-02 22:36:24 -0400

    Setup tests

--- test/scripts/compile/compile.test.ts
+++ test/scripts/compile/compile.test.ts
@@ -1,6 +1,27 @@
-import { describe, it } from 'vitest';
+import { beforeEach, describe, it } from 'vitest';
+import path from 'path';
+import fs from 'fs/promises';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'compile'
+);
+const OUT_DIR = path.join(WORKING_DIR, 'out');
 
 describe('compile file', () => {
+	beforeEach(async () => {
+		await fs
+			.readdir(OUT_DIR)
+			.then((files) =>
+				files
+					.filter((file) => '.gitkeep' !== file)
+					.map((file) => fs.rm(path.join(OUT_DIR, file)))
+			)
+			.then((promises) => Promise.all(promises));
+	});
+
 	it.fails('compiles ts file with esmodules');
 	it.fails('compiles ts file with commonjs');
 	it.fails('compiles js file with esmodules');
