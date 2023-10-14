3d433d59840c142affbd364c60ec7d1d1d620df1
Craig Miller <craigmiller160@gmail.com>
2023-10-14 09:35:41 -0400

    Test setup logic

--- test/scripts/_cypress/compileCypress.test.ts
+++ test/scripts/_cypress/compileCypress.test.ts
@@ -1,7 +1,33 @@
-import { describe, it } from 'vitest';
+import { afterEach, beforeEach, describe, it } from 'vitest';
+import fs from 'fs/promises';
+import path from 'path';
+
+const WORKING_DIR = path.join(
+	process.cwd(),
+	'test',
+	'__working_directories__',
+	'compileCypress'
+);
+
+const clean = (): Promise<unknown> =>
+	fs
+		.readdir(WORKING_DIR)
+		.then((files) =>
+			files
+				.filter((file) => file !== '.gitkeep')
+				.map((file) => path.join(WORKING_DIR, file))
+				.map((file) => fs.rm(file))
+		)
+		.then(Promise.all);
 
 describe('compile cypress config', () => {
-	it('compiles cypress config', () => {
-		throw new Error();
+	beforeEach(async () => {
+		await clean();
 	});
+
+	afterEach(async () => {
+		await clean();
+	});
+
+	it.fails('compiles cypress config');
 });
