c31eaac9bd6ce51f86f4c6b9ab8039ffa01cfc57
Craig Miller <craigmiller160@gmail.com>
2023-10-14 07:40:16 -0400

    Committing progress

--- test/scripts/cypress/compileCypress.test.ts
+++ test/scripts/cypress/compileCypress.test.ts
@@ -1,5 +1,7 @@
 import { describe, it } from 'vitest';
 
 describe('compile cypress config', () => {
-	it.fails('compiles cypress config');
+	it('compiles cypress config', () => {
+		throw new Error();
+	});
 });
