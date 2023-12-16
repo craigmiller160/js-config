a0965db921f4001c365941361ebe35207962f1ab
Craig Miller <craigmiller160@gmail.com>
2023-12-16 14:35:58 -0500

    Fixing tests

--- test/scripts/init/setupEslintPlugins.test.ts
+++ test/scripts/init/setupEslintPlugins.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { IsLibraryPresent } from '../../../src/scripts/utils/library';
 import { setupEslintPlugins } from '../../../src/scripts/init/setupEslintPlugins';
+import { EslintPlugins } from '../../../src/scripts/files/ControlFile';
 
 const createIsLibraryPresent =
 	(presentLibraries: ReadonlyArray<string>): IsLibraryPresent =>
@@ -11,31 +12,42 @@ describe('setupEslintPlugins', () => {
 	it('adds react plugins', () => {
 		const isLibraryPresent = createIsLibraryPresent(['react']);
 		const result = setupEslintPlugins(isLibraryPresent);
-		expect(result).toEqual([
-			'plugin:react/recommended',
-			'plugin:react-hooks/recommended',
-			'plugin:jsx-a11y/recommended',
-			'plugin:react/jsx-runtime'
-		]);
+		expect(result).toEqual<EslintPlugins>({
+			react: true,
+			testingLibraryReact: false,
+			tanstackQuery: false,
+			jestDom: false,
+			cypress: false,
+			vitest: false
+		});
 	});
 
 	it('adds vitest plugins', () => {
 		const isLibraryPresent = createIsLibraryPresent(['vitest']);
 		const result = setupEslintPlugins(isLibraryPresent);
-		expect(result).toEqual(['plugin:vitest/recommended']);
+		expect(result).toEqual<EslintPlugins>({
+			react: false,
+			testingLibraryReact: false,
+			tanstackQuery: false,
+			jestDom: false,
+			cypress: false,
+			vitest: true
+		});
 	});
 
 	it('adds testing library plugins', () => {
-		const expectedResult: ReadonlyArray<string> = [
-			'plugin:testing-library/dom',
-			'plugin:jest-dom/recommended'
-		];
-
 		const isLibraryPresentJestDom = createIsLibraryPresent([
 			'@testing-library/jest-dom'
 		]);
 		const jestDomResult = setupEslintPlugins(isLibraryPresentJestDom);
-		expect(jestDomResult).toEqual(expectedResult);
+		expect(jestDomResult).toEqual<EslintPlugins>({
+			react: false,
+			testingLibraryReact: false,
+			tanstackQuery: false,
+			jestDom: true,
+			cypress: false,
+			vitest: false
+		});
 	});
 
 	it('adds react testing library plugins', () => {
@@ -43,13 +55,27 @@ describe('setupEslintPlugins', () => {
 			'@testing-library/react'
 		]);
 		const reactResult = setupEslintPlugins(isLibraryPresentReact);
-		expect(reactResult).toEqual(['plugin:testing-library/react']);
+		expect(reactResult).toEqual<EslintPlugins>({
+			react: false,
+			testingLibraryReact: true,
+			tanstackQuery: false,
+			jestDom: false,
+			cypress: false,
+			vitest: false
+		});
 	});
 
 	it('adds cypress plugins', () => {
 		const isLibraryPresent = createIsLibraryPresent(['cypress']);
 		const result = setupEslintPlugins(isLibraryPresent);
-		expect(result).toEqual(['plugin:cypress/recommended']);
+		expect(result).toEqual<EslintPlugins>({
+			react: false,
+			testingLibraryReact: false,
+			tanstackQuery: false,
+			jestDom: false,
+			cypress: true,
+			vitest: false
+		});
 	});
 
 	it('adds tanstack query plugins', () => {
@@ -57,8 +83,13 @@ describe('setupEslintPlugins', () => {
 			'@tanstack/react-query'
 		]);
 		const result = setupEslintPlugins(isLibraryPresent);
-		expect(result).toEqual([
-			'plugin:@tanstack/eslint-plugin-query/recommended'
-		]);
+		expect(result).toEqual<EslintPlugins>({
+			react: false,
+			testingLibraryReact: false,
+			tanstackQuery: true,
+			jestDom: false,
+			cypress: false,
+			vitest: false
+		});
 	});
 });
