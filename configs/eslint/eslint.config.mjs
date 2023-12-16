335a01870ba086db5267c3f62c768e9b2496fea1
Craig Miller <craigmiller160@gmail.com>
2023-12-16 13:16:43 -0500

    Progress

--- configs/eslint/eslint.config.mjs
+++ configs/eslint/eslint.config.mjs
@@ -4,62 +4,87 @@ import eslintTs from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 import eslintPrettier from 'eslint-plugin-prettier';
 import eslintSonar from 'eslint-plugin-sonarjs';
+import path from 'path';
+import fs from 'fs';
+
+const controlFilePath = path.join(__dirname, '..', '..', 'control-file.json');
+const controlFile = JSON.parse(fs.readFileSync(controlFilePath, 'utf8'));
+const hasReact = controlFile.eslintPlugins.includes('plugin:react/recommended');
 
 // TODO explore import plugin again
 
-export default [
-	{
-		files: ['**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'],
-		languageOptions: {
-			ecmaVersion: 'latest',
-			globals: {
-				...globals.node
-			}
-		},
-		plugins: {
-			prettier: eslintPrettier,
-			sonarjs: eslintSonar
-		},
-		linterOptions: {
-			reportUnusedDisableDirectives: true
-		},
-		rules: {
-			...eslintJs.configs.recommended.rules,
-			...eslintSonar.configs.recommended.rules,
-			'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-			'no-console': [
-				'error',
-				{
-					allow: ['error']
+export default (async () => {
+	const defaultConfig = [
+		{
+			files: ['**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'],
+			languageOptions: {
+				ecmaVersion: 'latest',
+				globals: {
+					...globals.node
 				}
-			],
-			'sonarjs/no-duplicate-string': 0
-		}
-	},
-	{
-		files: ['**/*.{ts,tsx,mts,cts}'],
-		ignores: ['vite.config.{ts,mts,cts}'],
-		languageOptions: {
-			parser: parserTs,
-			parserOptions: {
-				project: true
+			},
+			plugins: {
+				prettier: eslintPrettier,
+				sonarjs: eslintSonar
+			},
+			linterOptions: {
+				reportUnusedDisableDirectives: true
+			},
+			rules: {
+				...eslintJs.configs.recommended.rules,
+				...eslintSonar.configs.recommended.rules,
+				'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+				'no-console': [
+					'error',
+					{
+						allow: ['error']
+					}
+				],
+				'sonarjs/no-duplicate-string': 0
 			}
 		},
-		plugins: {
-			'@typescript-eslint': eslintTs
+		{
+			files: ['**/*.{ts,tsx,mts,cts}'],
+			ignores: ['vite.config.{ts,mts,cts}'],
+			languageOptions: {
+				parser: parserTs,
+				parserOptions: {
+					project: true
+				}
+			},
+			plugins: {
+				'@typescript-eslint': eslintTs
+			},
+			rules: {
+				...eslintTs.configs['eslint-recommended'].overrides[0].rules,
+				// TODO need to have type-sensitive rules too
+				...eslintTs.configs.recommended.rules,
+				'@typescript-eslint/no-misused-promises': [
+					'error',
+					{
+						checksVoidReturn: {
+							arguments: false,
+							attributes: false,
+							properties: true,
+							returns: true,
+							variables: true
+						}
+					}
+				]
+			}
 		},
-		rules: {
-			...eslintTs.configs['eslint-recommended'].overrides[0].rules,
-			// TODO need to have type-sensitive rules too
-			...eslintTs.configs.recommended.rules
-		}
-	},
-	{
-		files: ['vite.config.{ts,mts,cts}'],
-		languageOptions: {
-			parserOptions: {
-				project: 'tsconfig.vite.json'
+		{
+			files: ['test/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}']
+		},
+		{
+			files: ['vite.config.{ts,mts,cts}'],
+			languageOptions: {
+				parserOptions: {
+					project: 'tsconfig.vite.json'
+				}
 			}
 		}
-	}
-];
+	];
+
+	return defaultConfig;
+})();
