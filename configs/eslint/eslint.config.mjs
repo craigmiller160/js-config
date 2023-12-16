c50423e9c356b5a905f173af3f654bad5ecd5113
Craig Miller <craigmiller160@gmail.com>
2023-12-16 13:42:06 -0500

    Added react configs

--- configs/eslint/eslint.config.mjs
+++ configs/eslint/eslint.config.mjs
@@ -7,85 +7,109 @@ import eslintSonar from 'eslint-plugin-sonarjs';
 import path from 'path';
 import fs from 'fs';
 import url from 'url';
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintJsxA11y from 'eslint-plugin-jsx-a11y';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const controlFilePath = path.join(__dirname, '..', '..', 'control-file.json');
 const controlFile = JSON.parse(fs.readFileSync(controlFilePath, 'utf8'));
-// const hasReact = controlFile.eslintPlugins.includes('plugin:react/recommended');
 
 // TODO explore import plugin again
 
-export default (async () => {
-	return [
-		{
-			files: ['**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'],
-			languageOptions: {
-				ecmaVersion: 'latest',
-				globals: {
-					...globals.node
-				}
-			},
-			plugins: {
-				prettier: eslintPrettier,
-				sonarjs: eslintSonar
-			},
-			linterOptions: {
-				reportUnusedDisableDirectives: true
-			},
-			rules: {
-				...eslintJs.configs.recommended.rules,
-				...eslintSonar.configs.recommended.rules,
-				'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-				'no-console': [
-					'error',
-					{
-						allow: ['error']
-					}
-				],
-				'sonarjs/no-duplicate-string': 0
+const eslintConfigs = [
+	{
+		files: ['**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			globals: {
+				...globals.node
 			}
 		},
-		{
-			files: ['**/*.{ts,tsx,mts,cts}'],
-			ignores: ['vite.config.{ts,mts,cts}'],
-			languageOptions: {
-				parser: parserTs,
-				parserOptions: {
-					project: true
+		plugins: {
+			prettier: eslintPrettier,
+			sonarjs: eslintSonar
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: true
+		},
+		rules: {
+			...eslintJs.configs.recommended.rules,
+			...eslintSonar.configs.recommended.rules,
+			'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+			'no-console': [
+				'error',
+				{
+					allow: ['error']
 				}
-			},
-			plugins: {
-				'@typescript-eslint': eslintTs
-			},
-			rules: {
-				...eslintTs.configs['eslint-recommended'].overrides[0].rules,
-				// TODO need to have type-sensitive rules too
-				...eslintTs.configs.recommended.rules,
-				'@typescript-eslint/no-misused-promises': [
-					'error',
-					{
-						checksVoidReturn: {
-							arguments: false,
-							attributes: false,
-							properties: true,
-							returns: true,
-							variables: true
-						}
-					}
-				]
+			],
+			'sonarjs/no-duplicate-string': 0
+		}
+	},
+	{
+		files: ['**/*.{ts,tsx,mts,cts}'],
+		ignores: ['vite.config.{ts,mts,cts}'],
+		languageOptions: {
+			parser: parserTs,
+			parserOptions: {
+				project: true
 			}
 		},
-		{
-			files: ['test/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}']
+		plugins: {
+			'@typescript-eslint': eslintTs
 		},
-		{
-			files: ['vite.config.{ts,mts,cts}'],
-			languageOptions: {
-				parserOptions: {
-					project: 'tsconfig.vite.json'
+		rules: {
+			...eslintTs.configs['eslint-recommended'].overrides[0].rules,
+			// TODO need to have type-sensitive rules too
+			...eslintTs.configs.recommended.rules,
+			'@typescript-eslint/no-misused-promises': [
+				'error',
+				{
+					checksVoidReturn: {
+						arguments: false,
+						attributes: false,
+						properties: true,
+						returns: true,
+						variables: true
+					}
 				}
+			]
+		}
+	},
+	{
+		files: ['test/**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}']
+	},
+	{
+		files: ['vite.config.{ts,mts,cts}'],
+		languageOptions: {
+			parserOptions: {
+				project: 'tsconfig.vite.json'
 			}
 		}
-	];
-})();
+	}
+];
+
+if (controlFile.eslintPlugins.react) {
+	eslintConfigs.push({
+		files: ['**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'],
+		plugins: {
+			react: eslintReact,
+			'react-hooks': eslintReactHooks,
+			'jsx-a11y': eslintJsxA11y
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		},
+		rules: {
+			...eslintReact.configs.recommended.rules,
+			...eslintReactHooks.configs.recommended.rules,
+			...eslintJsxA11y.configs.recommended.rules,
+			...eslintReact.configs['jsx-runtime'].rules
+		}
+	});
+}
+
+export default eslintConfigs;
