ceb161713f430d3e745b5bfe4d43bd222e59fc1d
Craig Miller <craigmiller160@gmail.com>
2023-12-16 14:03:38 -0500

    Completely refactored eslint command

--- src/scripts/c-eslint.ts
+++ src/scripts/c-eslint.ts
@@ -1,73 +1,35 @@
 import { runCommandSync } from './utils/runCommand';
 import { function as func, either } from 'fp-ts';
-import path from 'path';
-import fs from 'fs';
 import { terminate } from './utils/terminate';
 import { getRealArgs } from './utils/process';
 import { findCommand } from './utils/command';
 import { ESLINT } from './commandPaths';
 import { logger } from './logger';
+import { parseControlFile } from './files/ControlFile';
+import { match } from 'ts-pattern';
 
-const SRC_TEST_PATH = '{src,test}/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}';
-const CYPRESS_PATH = 'cypress/**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}';
-
-// TODO need to fix this to get the right config file extension
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Running eslint');
 	const args = getRealArgs(process);
 
-	const eslintEither = findCommand(process, ESLINT);
-	const cypressDir = path.join(process.cwd(), 'cypress');
-
-	if (args.length > 0) {
-		const noVitest = `${args[0].startsWith(cypressDir)}`;
-		func.pipe(
-			eslintEither,
-			either.chain((command) =>
-				runCommandSync(
-					`${command} --config eslint.config.mjs --fix --max-warnings=0 ${args[0]}`,
-					{
-						env: {
-							...process.env,
-							NO_VITEST: noVitest,
-							ESLINT_USE_FLAT_CONFIG: 'true'
-						}
-					}
-				)
-			),
-			either.fold(terminate, terminate)
-		);
-		return;
-	}
-
 	func.pipe(
-		eslintEither,
-		either.chainFirst((command) =>
+		parseControlFile(process),
+		either.map((controlFile) => controlFile.projectType),
+		either.map((type) =>
+			match(type)
+				.with('module', () => 'eslint.config.js')
+				.otherwise(() => 'eslint.config.mjs')
+		),
+		either.bindTo('configFile'),
+		either.bind('targetFile', () =>
+			either.right(args[0] ? args[0] : undefined)
+		),
+		either.bind('command', () => findCommand(process, ESLINT)),
+		either.chain(({ configFile, targetFile, command }) =>
 			runCommandSync(
-				`${command} --config eslint.config.mjs --fix --max-warnings=0 ${SRC_TEST_PATH}`,
-				{
-					env: {
-						...process.env,
-						ESLINT_USE_FLAT_CONFIG: 'true'
-					}
-				}
+				`${command} --config ${configFile} --fix --max-warnings=0 ${targetFile}`
 			)
 		),
-		either.chain((command) => {
-			if (fs.existsSync(cypressDir)) {
-				return runCommandSync(
-					`${command} --config eslint.config.mjs --fix --max-warnings=0 ${CYPRESS_PATH}`,
-					{
-						env: {
-							...process.env,
-							NO_VITEST: 'true',
-							ESLINT_USE_FLAT_CONFIG: 'true'
-						}
-					}
-				);
-			}
-			return either.right('');
-		}),
 		either.fold(terminate, terminate)
 	);
 };
