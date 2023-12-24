572204910e56e4223abd81b8df63c3bfaca322dc
Craig Miller <craigmiller160@gmail.com>
2023-12-24 11:40:03 -0500

    Cleanup and adding lib

--- src/scripts/init/setupEslintFiles.ts
+++ src/scripts/init/setupEslintFiles.ts
@@ -1,63 +1,11 @@
-import { either, taskEither } from 'fp-ts';
-import path from 'path';
-import fs from 'fs';
+import { taskEither } from 'fp-ts';
 import { logger } from '../logger';
-import { PackageJson, PackageJsonType } from '../files/PackageJson';
-
-const getEslintConfigPath = (cwd: string, type: PackageJsonType): string => {
-	if (type === 'commonjs') {
-		return path.join(cwd, 'eslint.config.mjs');
-	}
-	return path.join(cwd, 'eslint.config.js');
-};
-const getPrettierrcPath = (cwd: string, type: PackageJsonType): string => {
-	if (type === 'commonjs') {
-		return path.join(cwd, '.prettierrc.js');
-	}
-	return path.join(cwd, '.prettierrc.cjs');
-};
-
-const shouldWriteConfig = (configPath: string): boolean => {
-	if (fs.existsSync(configPath)) {
-		const config = fs.readFileSync(configPath, 'utf8');
-		return !config.includes('@craigmiller160/js-config');
-	}
-	return true;
-};
-
-const writeEslintFiles = (
-	cwd: string,
-	packageJson: PackageJson
-): either.Either<Error, void> =>
-	either.tryCatch(() => {
-		const eslintConfigPath = getEslintConfigPath(cwd, packageJson.type);
-		if (shouldWriteConfig(eslintConfigPath)) {
-			fs.writeFileSync(
-				eslintConfigPath,
-				`export { default } from '@craigmiller160/js-config/configs/eslint/eslint.config.mjs';`
-			);
-		}
-
-		const prettierrcPath = getPrettierrcPath(cwd, packageJson.type);
-		if (shouldWriteConfig(prettierrcPath)) {
-			fs.writeFileSync(
-				prettierrcPath,
-				`module.exports = require('@craigmiller160/js-config/configs/eslint/.prettierrc.js');`
-			);
-		}
-	}, either.toError);
-
-export const setupEslintFilesOld = (
-	cwd: string,
-	packageJson: PackageJson
-): either.Either<Error, void> => {
-	logger.info('Setting up eslint files');
-	return writeEslintFiles(cwd, packageJson);
-};
+import { PackageJson } from '../files/PackageJson';
 
 export const setupEslintFiles = (
 	cwd: string,
 	packageJson: PackageJson
 ): taskEither.TaskEither<Error, void> => {
+	logger.info('Setting up eslint files');
 	throw new Error();
 };
