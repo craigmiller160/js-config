f47acf761bdb44578d7a180f1e04a70f7be9dd8d
Craig Miller <craigmiller160@gmail.com>
2024-01-15 21:36:24 -0500

    Working on c-start command

--- src/scripts/c-start.ts
+++ src/scripts/c-start.ts
@@ -5,19 +5,34 @@ import { TSC, VITE } from './commandPaths';
 import { either, function as func } from 'fp-ts';
 import { terminate } from './utils/terminate';
 import { getRealArgs } from './utils/process';
-import path from 'path';
+import { PackageJsonType } from './files/PackageJson';
+import { match } from 'ts-pattern';
+import { parseControlFile } from './files/ControlFile';
+
+const getExtension = (type: PackageJsonType): string =>
+	match<PackageJsonType, string>(type)
+		.with('module', () => 'ts')
+		.with('commonjs', () => 'mts')
+		.exhaustive();
+
+const getConfigFile = (process: NodeJS.Process): either.Either<Error, string> =>
+	func.pipe(
+		parseControlFile(process),
+		either.map((controlFile) => getExtension(controlFile.projectType)),
+		either.map((ext) => `vite.config.${ext}`)
+	);
 
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Starting dev server');
 
 	const args = getRealArgs(process).join(' ');
-	const config = path.join(process.cwd(), 'vite.config.mts');
 
 	func.pipe(
 		findCommand(process, VITE),
 		either.bindTo('viteCommand'),
 		either.bind('tscCommand', () => findCommand(process, TSC)),
-		either.fold(terminate, ({ viteCommand, tscCommand }) => {
+		either.bind('config', () => getConfigFile(process)),
+		either.fold(terminate, ({ viteCommand, tscCommand, config }) => {
 			void runCommandAsync(`${viteCommand} ${args} -c ${config}`)();
 			void runCommandAsync(`${tscCommand} --noEmit --watch`)();
 		})
