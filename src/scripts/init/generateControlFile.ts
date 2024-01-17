6b4cb64a524320c698a28ec747b4f0711ae9fe14
Craig Miller <craigmiller160@gmail.com>
2024-01-17 16:54:27 -0500

    Refactoring control file generation to handle more directory structures

--- src/scripts/init/generateControlFile.ts
+++ src/scripts/init/generateControlFile.ts
@@ -4,9 +4,43 @@ import {
 	getLocalControlFile
 } from '../files/ControlFile';
 import fs from 'fs';
-import { either } from 'fp-ts';
-import { PackageJson } from '../files/PackageJson';
+import path from 'path';
+import { either, function as func, option } from 'fp-ts';
+import { PackageJson, parsePackageJson } from '../files/PackageJson';
 import { logger } from '../logger';
+
+const JS_CONFIG = '@craigmiller160/js-config';
+
+const checkDirectoryForJsConfig = (directory: string): boolean =>
+	func.pipe(
+		parsePackageJson(path.join(directory, 'package.json')),
+		option.fromEither,
+		option.filter((packageJson) => JS_CONFIG === packageJson.name),
+		option.isSome
+	);
+
+const findJsConfigDirectory = (
+	process: NodeJS.Process
+): either.Either<Error, string> => {
+	if (checkDirectoryForJsConfig(process.cwd())) {
+		return either.right(process.cwd());
+	}
+
+	const nodeModulesPath = path.join(
+		process.cwd(),
+		'node_modules',
+		'@craigmiller160',
+		'js-config'
+	);
+	if (checkDirectoryForJsConfig(nodeModulesPath)) {
+		return either.right(nodeModulesPath);
+	}
+	return either.left(
+		new Error(
+			`Cannot find js-config directory from starting directory of ${process.cwd()}`
+		)
+	);
+};
 
 export const generateControlFile = (
 	cwd: string,
@@ -26,13 +60,16 @@ export const generateControlFile = (
 			cypress: hasCypressDirectory
 		}
 	};
-	return either.tryCatch(
-		() =>
-			fs.writeFileSync(
-				// It's not really local, pnpm has cwd being the node_modules location of this lib
-				getLocalControlFile(process.cwd()),
-				JSON.stringify(controlFile, null, 2)
-			),
-		either.toError
+
+	const controlFileJson = JSON.stringify(controlFile, null, 2);
+	return func.pipe(
+		findJsConfigDirectory(process),
+		either.map(getLocalControlFile),
+		either.chain((controlFilePath) =>
+			either.tryCatch(
+				() => fs.writeFileSync(controlFilePath, controlFileJson),
+				either.toError
+			)
+		)
 	);
 };
