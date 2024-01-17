a2652aa36d56b76ad6dac8bc3691b4e8be59f4b3
Craig Miller <craigmiller160@gmail.com>
2024-01-17 16:59:08 -0500

    Fixed lint error

--- src/scripts/utils/library.ts
+++ src/scripts/utils/library.ts
@@ -1,13 +1,8 @@
 import { either, function as func } from 'fp-ts';
-import { logger } from '../logger';
 
 export const isLibraryPresent = (name: string): boolean =>
 	func.pipe(
 		either.tryCatch(() => require.resolve(name), func.identity),
-		either.mapLeft((ex) => {
-			logger.error(`EXCEPTION: ${ex}`); // TODO delete this
-			return ex;
-		}),
 		either.isRight
 	);
 
