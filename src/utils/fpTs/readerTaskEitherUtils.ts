f4c917aaa2dd1c4a305fe7e6d9ee2ca42369eb51
Craig Miller <craigmiller160@gmail.com>
2024-01-29 15:23:55 -0500

    Refactored names

--- src/utils/fpTs/readerTaskEitherUtils.ts
+++ src/utils/fpTs/readerTaskEitherUtils.ts
@@ -1,11 +1,16 @@
 import { readerTaskEither, reader, function as func } from 'fp-ts';
 
 export const narrowAndChainFirstReaderK =
-	<R1, R2, A, B>(f1: (r: R1) => R2, f2: (a: A) => reader.Reader<R2, B>) =>
+	<R1, R2, A, B>(
+		narrow: (r: R1) => R2,
+		chain: (a: A) => reader.Reader<R2, B>
+	) =>
 	<E>(
 		rte: readerTaskEither.ReaderTaskEither<R1, E, A>
 	): readerTaskEither.ReaderTaskEither<R1, E, A> =>
 		func.pipe(
 			rte,
-			readerTaskEither.chainFirstReaderK((a) => reader.local(f1)(f2(a)))
+			readerTaskEither.chainFirstReaderK((a) =>
+				reader.local(narrow)(chain(a))
+			)
 		);
