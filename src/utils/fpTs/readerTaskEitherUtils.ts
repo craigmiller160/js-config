007a6d6bdeedd0f420dbd5d0e28cc22b95c97057
Craig Miller <craigmiller160@gmail.com>
2024-01-29 15:26:53 -0500

    Added another utility function

--- src/utils/fpTs/readerTaskEitherUtils.ts
+++ src/utils/fpTs/readerTaskEitherUtils.ts
@@ -1,4 +1,9 @@
-import { readerTaskEither, reader, function as func } from 'fp-ts';
+import {
+	readerTaskEither,
+	readerEither,
+	reader,
+	function as func
+} from 'fp-ts';
 
 export const narrowAndChainFirstReaderK =
 	<R1, R2, A, B>(
@@ -12,5 +17,20 @@ export const narrowAndChainFirstReaderK =
 			rte,
 			readerTaskEither.chainFirstReaderK((a) =>
 				reader.local(narrow)(chain(a))
+			)
+		);
+
+export const narrowAndChainFirstReaderEitherK =
+	<R1, R2, E, A, B>(
+		narrow: (r: R1) => R2,
+		chain: (a: A) => readerEither.ReaderEither<R2, E, B>
+	) =>
+	(
+		rte: readerTaskEither.ReaderTaskEither<R1, E, A>
+	): readerTaskEither.ReaderTaskEither<R1, E, A> =>
+		func.pipe(
+			rte,
+			readerTaskEither.chainFirstReaderEitherK((a) =>
+				readerEither.local(narrow)(chain(a))
 			)
 		);
