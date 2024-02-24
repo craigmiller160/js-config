7a46670e9176274a7d485e51e36821a00e7f8e6b
Craig Miller <craigmiller160@gmail.com>
2024-02-24 16:10:09 -0500

    Integrated control file into validation

--- src/scripts/c-validate.ts
+++ src/scripts/c-validate.ts
@@ -2,23 +2,43 @@ import { logger } from './logger';
 import { either, function as func } from 'fp-ts';
 import { runCommandSync } from './utils/runCommand';
 import { terminate } from './utils/terminate';
-import path from 'path';
-import fs from 'fs';
+import { parseControlFile } from './files/ControlFile';
 
 export const execute = (process: NodeJS.Process) => {
 	logger.info('Running full validation');
+
+	parseControlFile(process);
+
 	func.pipe(
-		runCommandSync('c-type-check'),
-		either.chain(() => runCommandSync('c-eslint')),
-		either.chain(() => runCommandSync('c-stylelint')),
-		either.chain(() => runCommandSync('c-test')),
-		either.chain(() => {
-			const cypressPath = path.join(process.cwd(), 'cypress');
-			if (fs.existsSync(cypressPath)) {
-				return runCommandSync('c-cypress');
+		parseControlFile(process),
+		either.bindTo('controlFile'),
+		either.chainFirst(() => runCommandSync('c-type-check')),
+		either.chainFirst(() => runCommandSync('c-eslint')),
+		either.chainFirst(() => runCommandSync('c-stylelint')),
+		either.chainFirst(
+			({
+				controlFile: {
+					directories: { test }
+				}
+			}) => {
+				if (test) {
+					return runCommandSync('c-test');
+				}
+				return either.right('');
 			}
-			return either.right('');
-		}),
+		),
+		either.chainFirst(
+			({
+				controlFile: {
+					directories: { cypress }
+				}
+			}) => {
+				if (cypress) {
+					return runCommandSync('c-cypress');
+				}
+				return either.right('');
+			}
+		),
 		either.fold(terminate, terminate)
 	);
 };
