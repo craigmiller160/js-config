1acdb519798977c49d64eb8f9bdfb017f52b71e8
Craig Miller <craigmiller160@gmail.com>
2024-04-27 15:46:39 -0400

    More work on test

--- test/scripts/c-type-styles.test.ts
+++ test/scripts/c-type-styles.test.ts
@@ -1,8 +1,32 @@
-import { MockedFunction, vi, test } from 'vitest';
+import { MockedFunction, vi, test, beforeEach, expect } from 'vitest';
 import { RunCommandSync } from '../../src/scripts/utils/runCommand';
+import { execute } from '../../src/scripts/c-type-styles';
+import { either } from 'fp-ts';
+import {
+	TYPED_CSS_MODULES,
+	TYPED_SCSS_MODULES
+} from '../../src/scripts/commandPaths';
 
 const mockRunCommandSync: MockedFunction<RunCommandSync> = vi.fn();
 
+beforeEach(() => {
+	vi.resetAllMocks;
+});
+
 test('generates types for css & scss files', () => {
-    throw new Error();
+	mockRunCommandSync.mockReturnValue(either.right(''));
+	execute({
+		process,
+		runCommandSync: mockRunCommandSync
+	});
+
+	expect(mockRunCommandSync).toHaveBeenCalledTimes(2);
+	expect(mockRunCommandSync).toHaveBeenNthCalledWith(
+		1,
+		`${TYPED_CSS_MODULES} -p 'src/**/*.module.css'`
+	);
+	expect(mockRunCommandSync).toHaveBeenNthCalledWith(
+		2,
+		`${TYPED_SCSS_MODULES} 'src/**/*.module.scss'`
+	);
 });
