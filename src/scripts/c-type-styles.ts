91ed0532c5508ddd2458f86ff42202019b29d37a
Craig Miller <craigmiller160@gmail.com>
2024-04-27 15:54:34 -0400

    Some more progress with file paths

--- src/scripts/c-type-styles.ts
+++ src/scripts/c-type-styles.ts
@@ -7,6 +7,7 @@ import { function as func, either } from 'fp-ts';
 import { findCommand } from './utils/command';
 import { TYPED_CSS_MODULES, TYPED_SCSS_MODULES } from './commandPaths';
 import { terminate } from './utils/terminate';
+import path from 'path';
 
 export type Dependencies = Readonly<{
 	process: NodeJS.Process;
@@ -22,19 +23,21 @@ export const execute = (
 	const { process, runCommandSync } = dependencies;
 	logger.info('Generating types for CSS/SCSS modules');
 
+	const basePathPattern = path.join(process.cwd(), 'src', '**');
+	const cssPathPattern = path.join(basePathPattern, '*.module.css');
+	const scssPathPattern = path.join(basePathPattern, '*.module.scss');
+
 	const runTypeCssModules = () =>
 		func.pipe(
 			findCommand(process, TYPED_CSS_MODULES),
 			either.chain((cmd) =>
-				runCommandSync(`${cmd} -p 'src/**/*.module.css'`)
+				runCommandSync(`${cmd} -p '${cssPathPattern}'`)
 			)
 		);
 	const runTypeScssModules = () =>
 		func.pipe(
 			findCommand(process, TYPED_SCSS_MODULES),
-			either.chain((cmd) =>
-				runCommandSync(`${cmd} 'src/**/*.module.scss'`)
-			)
+			either.chain((cmd) => runCommandSync(`${cmd} '${scssPathPattern}'`))
 		);
 
 	func.pipe(
