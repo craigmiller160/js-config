9f6734ef15d39aba40365a50eec80f9843784ef4
Craig Miller <craigmiller160@gmail.com>
2024-05-19 13:03:08 -0400

    Fixed handling of test support file

--- src/scripts/init/setupTypescript.ts
+++ src/scripts/init/setupTypescript.ts
@@ -48,10 +48,11 @@ const createTestSupportTypes =
         const fileContent = [jestFpTsImport, jestDomImport]
             .filter((item): item is string => !!item)
             .join('\n');
+        const formattedFileContent = `${fileContent}\n`;
 
         return either.tryCatch(() => {
             if (fileContent.trim().length > 0) {
-                fs.writeFileSync(supportFilePath, fileContent);
+                fs.writeFileSync(supportFilePath, formattedFileContent);
             } else if (fs.existsSync(supportFilePath)) {
                 fs.rmSync(supportFilePath);
             }
